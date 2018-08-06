86ebceee5b4bbbcfe2f00cb4ca728faa8685cd38
kipill <kipill_90@mail.ru>
2018-08-06 23:59:39 +0200

    126. Word Ladder II

--- src/main/java/com/leetcode/problems/array/hard/WordLadderII.java
+++ src/main/java/com/leetcode/problems/array/hard/WordLadderII.java
@@ -7,114 +7,102 @@ import java.util.*;
  */
 public class WordLadderII {
 
-	public class Solution {
-		public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-			if (!wordList.contains(endWord)) {
-				return Collections.emptyList();
-			}
-			Map<String, List<String>> options = new HashMap<>();
-			options.put(beginWord, findAllSimilar(beginWord, wordList));
-			for (String word : wordList) {
-				if (!word.equals(endWord)) {
-					options.put(word, findAllSimilar(word, wordList));
-				}
-			}
+    public class Solution {
 
-			int depth = findDepth(beginWord, endWord, options);
-			if (depth == Integer.MAX_VALUE) {
-				return Collections.emptyList();
-			}
-			LinkedList<String> stack = new LinkedList<>();
-			stack.addLast(beginWord);
-			int[] visited = new int[wordList.size()];
-			Arrays.fill(visited, Integer.MAX_VALUE);
+        public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+            if (!wordList.contains(endWord)) {
+                return Collections.emptyList();
+            }
+            Map<String, Set<String>> options = new HashMap<>();
+            options.put(beginWord, findAllSimilar(beginWord, wordList));
+            for (String word : wordList) {
+                if (!word.equals(endWord)) {
+                    options.put(word, findAllSimilar(word, wordList));
+                }
+            }
 
-			List<List<String>> result = new ArrayList<>();
-			depthFirst(stack, depth, options, result, endWord, visited, wordList);
-			return result;
-		}
+            Map<String, List<String>> map = new HashMap<>();
+            Queue<String> queue = new ArrayDeque<>();
+            Set<String> unvisited = new HashSet<>(wordList);
+            Set<String> visited = new HashSet<>();
 
-		private void depthFirst(LinkedList<String> stack,
-								int depth,
-								Map<String, List<String>> options,
-								List<List<String>> result,
-								String endWord,
-								int[] visited,
-								List<String> wordList) {
-			if (stack.isEmpty()) {
-				return;
-			}
-			String current = stack.peekLast();
-			if (stack.size() + 1 == depth) {
-				if (options.get(current).contains(endWord)) {
-					List<String> way = new ArrayList<>(stack);
-					way.add(endWord);
-					result.add(way);
-				}
-			} else if (stack.size() + 1 < depth) {
-				for (String next : options.get(current)) {
-					int idx = wordList.indexOf(next);
-					if (idx == -1 || visited[idx] > stack.size()) {
-						visited[idx] = stack.size();
-						stack.addLast(next);
-						depthFirst(stack, depth, options, result, endWord, visited, wordList);
-					}
-				}
-			}
-			int idx = wordList.indexOf(current);
-			if (idx != -1) {
-				visited[idx] = stack.size();
-			}
-			stack.pollLast();
-		}
+            queue.add(beginWord);
+            unvisited.add(endWord);
+            unvisited.remove(beginWord);
 
-		private int findDepth(String current, String end, Map<String, List<String>> options) {
-			Set<String> visited = new HashSet<>();
-			Set<String> next = new HashSet<>();
-			next.add(current);
+            int currentStepSize = 1;
+            int nextStepSize = 0;
+            boolean found = false;
 
-			int i = 1;
-			for (; !next.contains(end); i++) {
-				Set<String> tmp = new HashSet<>();
-				for (String key : next) {
-					if (!visited.contains(key)) {
-						tmp.addAll(options.getOrDefault(key, Collections.emptyList()));
-						visited.add(key);
-					}
-				}
-				next.clear();
-				next.addAll(tmp);
-				if (i > options.size()) {
-					return Integer.MAX_VALUE;
-				}
-			}
-			return i;
-		}
+            while (!queue.isEmpty()) {
+                String current = queue.poll();
+                currentStepSize--;
+                for (String nextWord : options.getOrDefault(current, new HashSet<>())) {
+                    if (unvisited.contains(nextWord)) {
+                        if (visited.add(nextWord)) {
+                            nextStepSize++;
+                            queue.add(nextWord);
+                        }
 
-		private List<String> findAllSimilar(String word, List<String> wordList) {
-			List<String> list = new ArrayList<>();
-			for (String option : wordList) {
-				if (option.equals(word)) {
-					continue;
-				}
-				boolean diff = false;
-				boolean add = true;
-				for (int i = 0; i < word.length(); i++) {
-					if (word.charAt(i) != option.charAt(i)) {
-						if (diff) {
-							add = false;
-							break;
-						} else {
-							diff = true;
-						}
-					}
-				}
-				if (add) {
-					list.add(option);
-				}
-			}
-			return list;
-		}
-	}
+                        map.computeIfAbsent(nextWord, ignored -> new ArrayList<>()).add(current);
+                        found = nextWord.equals(endWord);
+                    }
+                }
+                if (currentStepSize == 0) {
+                    if (found) {
+                        break;
+                    }
+                    currentStepSize = nextStepSize;
+                    nextStepSize = 0;
+                    unvisited.removeAll(visited);
+                    visited.clear();
+                }
+            }
+
+            List<List<String>> results = new ArrayList<>();
+            backtrace(endWord, beginWord, new LinkedList<>(), results, map);
+            return results;
+        }
+
+        private void backtrace(String word, String start, List<String> list, List<List<String>> results, Map<String, List<String>> map) {
+            if (word.equals(start)) {
+                List<String> way = new ArrayList<>(list.size() + 1);
+                way.add(start);
+                way.addAll(list);
+                results.add(way);
+            } else {
+                list.add(0, word);
+                for (String s : map.getOrDefault(word, new ArrayList<>())) {
+                    backtrace(s, start, list, results, map);
+                }
+                list.remove(0);
+            }
+        }
+
+        private Set<String> findAllSimilar(String word, List<String> wordList) {
+            Set<String> set = new HashSet<>();
+            for (String option : wordList) {
+                if (option.equals(word)) {
+                    continue;
+                }
+                boolean diff = false;
+                boolean add = true;
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) != option.charAt(i)) {
+                        if (diff) {
+                            add = false;
+                            break;
+                        } else {
+                            diff = true;
+                        }
+                    }
+                }
+                if (add) {
+                    set.add(option);
+                }
+            }
+            return set;
+        }
+    }
 
 }
