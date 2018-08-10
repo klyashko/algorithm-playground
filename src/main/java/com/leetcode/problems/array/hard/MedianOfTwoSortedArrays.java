7a656fbc53234849fc645979aaac4b315f38e27c
kipill <kipill_90@mail.ru>
2018-08-10 21:36:28 +0200

    4. Median of Two Sorted Arrays

--- src/main/java/com/leetcode/problems/array/hard/MedianOfTwoSortedArrays.java
+++ src/main/java/com/leetcode/problems/array/hard/MedianOfTwoSortedArrays.java
@@ -1,125 +1,43 @@
 package com.leetcode.problems.array.hard;
 
-/**
- * https://leetcode.com/problems/median-of-two-sorted-arrays/
- */
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MedianOfTwoSortedArrays {
 
     public class Solution {
         public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            if (nums1.length != 0 && nums2.length == 0) {
-                int middle = nums1.length / 2;
-                if (nums1.length % 2 == 0) {
-                    return (nums1[middle - 1] + nums1[middle]) / 2.;
-                } else {
-                    return nums1[middle];
-                }
-            } else if (nums1.length == 0 && nums2.length != 0) {
-                int middle = nums2.length / 2;
-                if (nums2.length % 2 == 0) {
-                    return (nums2[middle - 1] + nums2[middle]) / 2.;
-                } else {
-                    return nums2[middle];
-                }
-            } else if (nums1.length == 1 && nums2.length == 1) {
-                return (nums1[0] + nums2[0]) / 2.;
-            } else if (nums1.length == 1 && nums2.length == 2) {
-                return Math.min(Math.max(nums1[0], nums2[0]), nums2[1]);
-            } else if (nums1.length == 2 && nums2.length == 1) {
-                return Math.min(Math.max(nums1[0], nums2[0]), nums1[1]);
-            }
-            int length = nums1.length + nums2.length;
-            int middle = length / 2;
-            if (length % 2 == 0) {
-                middle--;
-            }
-            int i = 0, j = 0;
-            int lastUpdate = -1;
-
-            while ((i + j) < middle) {
-                if (i + 1 < nums1.length && j + 1 < nums2.length) {
-                    if (nums1[i] < nums2[j]) {
-                        i++;
-                        lastUpdate = 0;
+            PriorityQueue<Integer> q1 = new PriorityQueue<>();
+            PriorityQueue<Integer> q2 = new PriorityQueue<>(Comparator.reverseOrder());
+            int i = nums1.length - 1, j = nums2.length - 1;
+            while (i > -1 || j > -1) {
+                int toInsert;
+                if (i > -1 && j > -1) {
+                    if (nums1[i] > nums2[j]) {
+                        toInsert = nums1[i--];
                     } else {
-                        j++;
-                        lastUpdate = 1;
+                        toInsert = nums2[j--];
                     }
-                } else if (i + 1 < nums1.length) {
-                    i++;
-                    lastUpdate = 0;
+                } else if (i > -1) {
+                    toInsert = nums1[i--];
                 } else {
-                    j++;
-                    lastUpdate = 1;
+                    toInsert = nums2[j--];
+                }
+
+                if (q1.size() == q2.size()) {
+                    q2.add(toInsert);
+                } else {
+                    q1.add(q2.poll());
+                    q2.add(toInsert);
                 }
             }
-
-            System.out.println(i);
-            System.out.println(j);
-            System.out.println(middle);
-            System.out.println(lastUpdate);
-
-            if (length % 2 == 0) {
-                int first, second;
-                if (nums1[i] < nums2[j]) {
-                    System.out.println("first = nums1[i]");
-                    first = nums1[i];
-                    if (lastUpdate == 0) {
-                        i++;
-                    } else {
-                        lastUpdate = 0;
-                    }
-                } else {
-                    first = nums2[j];
-                    if (lastUpdate == 1) {
-                        j++;
-                    } else {
-                        lastUpdate = 1;
-                    }
-                }
-//                System.out.println(i);
-//                System.out.println(j);
-                if (i + 1 < nums1.length && j + 1 < nums2.length) {
-//                    System.out.println("i + 1 < nums1.length && j + 1 < nums2.length");
-                    if (lastUpdate == 0) {
-                        i++;
-                    } else {
-                        j++;
-                    }
-                    if (nums1[i] < nums2[j]) {
-                        second = nums1[i];
-                    } else {
-                        second = nums2[j];
-                    }
-                } else if (i + 1 < nums1.length) {
-//                    System.out.println("i + 1 < nums1.length");
-                    if (lastUpdate == 0) {
-                        i++;
-                    }
-                    second = nums1[i];
-                } else if (j + 1 < nums2.length) {
-//                    System.out.println("else");
-                    if (lastUpdate == 1) {
-                        j++;
-                    }
-                    second = nums2[j];
-                } else {
-                    second = lastUpdate == 0 ? nums2[j] : nums1[i];
-                }
-
-                System.out.println(first);
-                System.out.println(second);
-                return (first + second) / 2.;
-
+            if (q2.size() > q1.size()) {
+                return q2.poll();
             } else {
-//                if (lastUpdate == 0) {
-//                    return nums1[i];
-//                } else {
-//                    return nums2[j];
-//                }
-                return nums1[i] < nums2[j] ? nums1[i] : nums2[j];
+                return (q1.poll() + q2.poll()) / 2.;
             }
         }
+
     }
 
 }
--- src/test/groovy/com/leetcode/problems/array/hard/MedianOfTwoSortedArraysSpec.groovy
+++ src/test/groovy/com/leetcode/problems/array/hard/MedianOfTwoSortedArraysSpec.groovy
@@ -1,6 +1,6 @@
 package com.leetcode.problems.array.hard
 
-import com.leetcode.problems.array.hard.MedianOfTwoSortedArrays.Solution;
+import com.leetcode.problems.array.hard.MedianOfTwoSortedArrays.Solution
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -16,18 +16,18 @@ class MedianOfTwoSortedArraysSpec extends Specification {
         where:
 
             nums1     || nums2              || median
-//            [1, 3]    || [2]                || 2.0d
-//            [1, 2]    || [3, 4]             || 2.5d
-//            [2]       || []                 || 2
-//            [1, 2, 3] || []                 || 2
-//            []        || [2, 3]             || 2.5
-//            []        || [2, 3, 4, 5, 6, 7] || 4.5
-//            [3]       || [-2, -1]           || -1
-//            [1, 2]    || [-1, 3]            || 1.5
-//            [1]       || [1]                || 1
-//            [3]       || [1, 2, 4]          || 2.5d
-//            [100000]  || [100001]           || 100000.5d
-//            [1]       || [2, 3]             || 2
+            [1, 3]    || [2]                || 2.0d
+            [1, 2]    || [3, 4]             || 2.5d
+            [2]       || []                 || 2
+            [1, 2, 3] || []                 || 2
+            []        || [2, 3]             || 2.5
+            []        || [2, 3, 4, 5, 6, 7] || 4.5
+            [3]       || [-2, -1]           || -1
+            [1, 2]    || [-1, 3]            || 1.5
+            [1]       || [1]                || 1
+            [3]       || [1, 2, 4]          || 2.5d
+            [100000]  || [100001]           || 100000.5d
+            [1]       || [2, 3]             || 2
             [1]       || [2, 3, 4]          || 2.5
 
     }
