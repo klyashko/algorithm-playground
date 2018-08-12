031fde34a680afe078de2066a1049d8f1ffafb8e
kipill <kipill_90@mail.ru>
2018-08-12 19:31:55 +0200

    41. First Missing Positive

---
+++ src/test/groovy/com/leetcode/problems/array/hard/FirstMissingPositiveSpec.groovy
@@ -0,0 +1,27 @@
+package com.leetcode.problems.array.hard
+
+import com.leetcode.problems.array.hard.FirstMissingPositive.Solution;
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class FirstMissingPositiveSpec extends Specification {
+
+    @Unroll
+    def "firstMissingPositive"() {
+
+        expect:
+
+            new Solution().firstMissingPositive(input as int[]) == output
+
+        where:
+
+            input             || output
+            [1, 2, 0]         || 3
+            [3, 4, -1, 1]     || 2
+            [7, 8, 9, 11, 12] || 1
+            [1]               || 2
+            [1, 1]            || 2
+
+    }
+
+}
