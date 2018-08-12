50fafe4f9315586800e6426109cae3a401ecb2bc
kipill <kipill_90@mail.ru>
2018-08-12 18:38:51 +0200

    45. Jump Game II

--- src/main/java/com/leetcode/problems/array/hard/JumpGameII.java
+++ src/main/java/com/leetcode/problems/array/hard/JumpGameII.java
@@ -7,13 +7,8 @@ public class JumpGameII {
 
     class Solution {
         public int jump(int[] nums) {
-            //Already there :)
-            if (nums.length == 1) {
-                return 0;
-            }
             int jumps = 0;
-
-            for (int i = 0; i < nums.length; ) {
+            for (int i = 0; i < nums.length - 1; ) {
                 jumps++;
                 if (i + nums[i] + 1 >= nums.length) {
                     return jumps;
