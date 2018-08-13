6af23b1604546546162e5045fb6aad074e0f8133
kipill <kipill_90@mail.ru>
2018-08-13 23:20:58 +0200

    303. Range Sum Query - Immutable

---
+++ src/test/groovy/com/leetcode/problems/dynamic/programming/easy/RangeSumQueryImmutableSpec.groovy
@@ -0,0 +1,23 @@
+package com.leetcode.problems.dynamic.programming.easy
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class RangeSumQueryImmutableSpec extends Specification {
+
+    @Unroll
+    def "sumRange"() {
+
+        expect:
+
+            new NumArray(input as int[]).sumRange(i, j) == output
+
+        where:
+
+            input                 || i || j || output
+            [-2, 0, 3, -5, 2, -1] || 0 || 2 || 1
+            [-2, 0, 3, -5, 2, -1] || 2 || 5 || -1
+            [-2, 0, 3, -5, 2, -1] || 0 || 5 || -3
+    }
+
+}
