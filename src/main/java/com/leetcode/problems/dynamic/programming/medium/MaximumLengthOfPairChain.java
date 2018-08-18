0817f0ca35efbce68e391808bb093ab7175470a8
kipill <kipill_90@mail.ru>
2018-08-18 19:54:42 +0200

    646. Maximum Length of Pair Chain

--- src/main/java/com/leetcode/problems/dynamic/programming/medium/MaximumLengthOfPairChain.java
+++ src/main/java/com/leetcode/problems/dynamic/programming/medium/MaximumLengthOfPairChain.java
@@ -8,17 +8,39 @@ import java.util.Comparator;
  */
 public class MaximumLengthOfPairChain {
 
-    class Solution {
+    class SolutionGreedy {
         public int findLongestChain(int[][] pairs) {
             Arrays.sort(pairs, Comparator.comparingInt(p -> p[1]));
             int current = Integer.MIN_VALUE, length = 0;
             for (int[] pair : pairs) {
-                if(pair[0] > current){
+                if (pair[0] > current) {
                     current = pair[1];
                     length++;
                 }
             }
             return length;
+        }
+    }
+
+    class Solution {
+        public int findLongestChain(int[][] pairs) {
+            Arrays.sort(pairs, Comparator.comparingInt(p -> p[0]));
+            int[] dp = new int[pairs.length];
+            Arrays.fill(dp, 1);
+
+            for (int i = 1; i < pairs.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (pairs[j][1] < pairs[i][0]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            int max = 0;
+            for (int i : dp) {
+                max = Math.max(max, i);
+            }
+            return max;
         }
     }
 
