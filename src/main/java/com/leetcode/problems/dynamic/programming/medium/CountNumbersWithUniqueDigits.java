dd3c4066bb7fefc95014d02c924c80f2f037cbbd
kipill <kipill_90@mail.ru>
2018-08-19 17:54:21 +0200

    357. Count Numbers with Unique Digits

--- src/main/java/com/leetcode/problems/dynamic/programming/medium/CountNumbersWithUniqueDigits.java
+++ src/main/java/com/leetcode/problems/dynamic/programming/medium/CountNumbersWithUniqueDigits.java
@@ -10,9 +10,6 @@ public class CountNumbersWithUniqueDigits {
             if (n == 0) {
                 return 1;
             }
-            if (n == 1) {
-                return 10;
-            }
             int options = 9;
             for (int i = 1; i < n; i++) {
                 options *= (10 - i);
