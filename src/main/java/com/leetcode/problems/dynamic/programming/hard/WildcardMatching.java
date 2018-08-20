d2536484a6910ad24d42ce2633e45f0dfaef6ee7
GD2LYAS <kirill.lyashko@partner.commerzsystems.com>
2018-08-20 13:06:53 +0200

    44. Wildcard Matching

--- src/main/java/com/leetcode/problems/dynamic/programming/hard/WildcardMatching.java
+++ src/main/java/com/leetcode/problems/dynamic/programming/hard/WildcardMatching.java
@@ -14,58 +14,53 @@ public class WildcardMatching {
 
 		public boolean isMatch(String s, String p) {
 			String key = s + "_" + p;
-			if (cache.containsKey(key)) {
-				return cache.get(key);
-			}
-			if ("*".equals(p)) {
-				return true;
-			} else if (s.length() == 1 && "?".equals(p)) {
-				return true;
-			}
-			boolean result = false;
-			int cursor = 0;
-			int i = 0;
-			for (; i < p.length(); i++) {
-				if (cursor >= s.length()) {
-					while (i < p.length() && p.charAt(i) == '*') {
-						i++;
-					}
-					result = i == p.length();
-					break;
-				} else if ('?' == p.charAt(i)) {
-					cursor++;
-					continue;
-				} else if ('*' == p.charAt(i)) {
-					int offset = 0;
-					while (i < p.length() && ('*' == p.charAt(i) || '?' == p.charAt(i))) {
-						if ('?' == p.charAt(i)) {
-							offset++;
-						}
-						i++;
-					}
-					if (i == p.length()) {
-						result = offset == 0 || s.length() - cursor >= offset;
-						break;
-					}
-					if (offset > s.length() - cursor) {
-						break;
-					}
-					cursor += offset;
-					while (cursor < s.length() && !result) {
-						if (s.charAt(cursor) == p.charAt(i)) {
-							result = isMatch(s.substring(cursor), p.substring(i));
-						}
-						cursor++;
-					}
-					break;
-				} else if (s.charAt(cursor) != p.charAt(i)) {
-					break;
+			return cache.computeIfAbsent(key, ignored -> {
+				if ("*".equals(p)) {
+					return true;
+				} else if (s.length() == 1 && "?".equals(p)) {
+					return true;
 				}
-				cursor++;
-			}
-			result = result || (cursor == s.length() && i == p.length());
-			cache.put(key, result);
-			return result;
+				boolean result = false;
+				int cursor = 0;
+				int i = 0;
+				for (; i < p.length(); i++) {
+					if (cursor >= s.length()) {
+						while (i < p.length() && p.charAt(i) == '*') {
+							i++;
+						}
+						return i == p.length();
+					} else if ('?' == p.charAt(i)) {
+						cursor++;
+						continue;
+					} else if ('*' == p.charAt(i)) {
+						int offset = 0;
+						while (i < p.length() && ('*' == p.charAt(i) || '?' == p.charAt(i))) {
+							if ('?' == p.charAt(i)) {
+								offset++;
+							}
+							i++;
+						}
+						if (i == p.length()) {
+							return offset == 0 || s.length() - cursor >= offset;
+						}
+						if (offset > s.length() - cursor) {
+							return false;
+						}
+						cursor += offset;
+						while (cursor < s.length() && !result) {
+							if (s.charAt(cursor) == p.charAt(i)) {
+								result = isMatch(s.substring(cursor), p.substring(i));
+							}
+							cursor++;
+						}
+						break;
+					} else if (s.charAt(cursor) != p.charAt(i)) {
+						return false;
+					}
+					cursor++;
+				}
+				return result || (cursor == s.length() && i == p.length());
+			});
 		}
 	}
 
