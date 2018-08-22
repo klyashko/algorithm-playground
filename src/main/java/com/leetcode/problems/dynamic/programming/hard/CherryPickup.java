e7239281cfd3207e48f615dbe3bf0680238b11c3
GD2LYAS <kirill.lyashko@partner.commerzsystems.com>
2018-08-22 14:15:56 +0200

    741. Cherry Pickup

--- src/main/java/com/leetcode/problems/dynamic/programming/hard/CherryPickup.java
+++ src/main/java/com/leetcode/problems/dynamic/programming/hard/CherryPickup.java
@@ -7,105 +7,46 @@ import java.util.Arrays;
  */
 public class CherryPickup {
 
-    class Solution {
-        public int cherryPickup(int[][] grid) {
-            if (grid.length == 0) {
-                return 0;
-            }
-            int rows = grid.length;
-            int cols = grid[0].length;
-            int[][] profit = new int[rows][cols];
+	class Solution {
+		int[][][] memo;
 
-            profit[0][0] = grid[0][0];
+		int[][] grid;
 
-            for (int i = 1; i < cols; i++) {
-                if (grid[0][i] == -1) {
-                    break;
-                }
-                profit[0][i] = profit[0][i - 1] + grid[0][i];
-                profit[0][i - 1] = 0;
-            }
+		int N;
 
-            for (int i = 1; i < rows; i++) {
-                if (grid[i][0] == -1) {
-                    break;
-                }
-                profit[i][0] = profit[i - 1][0] + grid[i][0];
-                profit[i - 1][0] = 0;
-            }
+		public int cherryPickup(int[][] grid) {
+			this.grid = grid;
+			N = grid.length;
+			memo = new int[N][N][N];
+			for (int[][] layer : memo) {
+				for (int[] row : layer) {
+					Arrays.fill(row, Integer.MIN_VALUE);
+				}
+			}
+			return Math.max(0, dp(0, 0, 0));
+		}
 
-            for (int r = 1; r < rows; r++) {
-                for (int c = 1; c < cols; c++) {
-                    if (grid[r][c] == -1) {
-                        continue;
-                    }
-                    if (grid[r - 1][c] == -1 && grid[r][c - 1] == -1) {
-                        grid[r][c] = -1;
-                        continue;
-                    }
-                    if (profit[r - 1][c] + grid[r][c] > profit[r][c - 1] + grid[r][c]) {
-                        profit[r][c] = profit[r - 1][c] + grid[r][c];
-                        profit[r - 1][c] = 0;
-                    } else {
-                        profit[r][c] = profit[r][c - 1] + grid[r][c];
-                        profit[r][c - 1] = 0;
-                    }
-                }
-            }
-
-            if (profit[rows - 1][cols - 1] == 0) {
-                return 0;
-            }
-
-            int[][] profit2 = new int[rows][cols];
-//            profit2[rows - 1][cols - 1] = profit[rows - 1][cols - 1];
-            profit2[rows - 1][cols - 1] = 0;
-
-//            for (int i = cols - 2; i >= 0; i--) {
-//                if (grid[rows - 1][i] == -1) {
-//                    continue;
-//                }
-//                profit2[rows - 1][i] = profit2[rows - 1][i + 1] + profit[rows - 1][i];
-//            }
-//
-//            for (int i = rows - 2; i >= 0; i--) {
-//                if (grid[i][cols - 1] == -1) {
-//                    continue;
-//                }
-//                profit2[i][cols - 1] = profit2[i + 1][cols - 1] + profit[i][cols - 1];
-//            }
-
-            for (int r = rows - 2; r >= 0; r--) {
-                for (int c = cols - 2; c >= 0; c--) {
-                    if (grid[r][c] == -1) {
-                        continue;
-                    }
-                    if (profit2[r + 1][c] + profit[r][c] < profit2[r][c + 1] + profit[r][c]) {
-                        profit2[r][c] = profit2[r][c + 1] + profit[r][c];
-                        profit2[r][c + 1] = 0;
-                    } else {
-                        profit2[r][c] = profit2[r + 1][c] + profit[r][c];
-                        profit2[r + 1][c] = 0;
-                    }
-                }
-
-                for (int[] t : profit2) {
-                    System.out.println(Arrays.toString(t));
-                }
-                System.out.println();
-
-            }
-//
-            for (int[] t : profit) {
-                System.out.println(Arrays.toString(t));
-            }
-            System.out.println();
-            for (int[] t : profit2) {
-                System.out.println(Arrays.toString(t));
-            }
-
-            return profit2[0][0];
-        }
-    }
+		private int dp(int r1, int c1, int c2) {
+			int r2 = r1 + c1 - c2;
+			if (N == r1 || N == r2 || N == c1 || N == c2 || grid[r1][c1] == -1 || grid[r2][c2] == -1) {
+				return -9999;
+			} else if (r1 == N - 1 && c1 == N - 1) {
+				return grid[r1][c1];
+			} else if (memo[r1][c1][c2] != Integer.MIN_VALUE) {
+				return memo[r1][c1][c2];
+			} else {
+				int ans = grid[r1][c1];
+				if (c1 != c2) {
+					ans += grid[r2][c2];
+				}
+				ans += Math.max(
+					Math.max(dp(r1, c1 + 1, c2 + 1), dp(r1 + 1, c1, c2 + 1)),
+					Math.max(dp(r1, c1 + 1, c2), dp(r1 + 1, c1, c2))
+				);
+				memo[r1][c1][c2] = ans;
+				return ans;
+			}
+		}
+	}
 
 }
--- src/test/groovy/com/leetcode/problems/dynamic/programming/hard/CherryPickupSpec.groovy
+++ src/test/groovy/com/leetcode/problems/dynamic/programming/hard/CherryPickupSpec.groovy
@@ -2,22 +2,25 @@ package com.leetcode.problems.dynamic.programming.hard
 
 import com.leetcode.problems.dynamic.programming.hard.CherryPickup.Solution as S
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class CherryPickupSpec extends Specification {
 
-    def "cherryPickup"() {
+	@Unroll
+	def "cherryPickup"() {
 
-        expect:
+		expect:
 
-            new S().cherryPickup(input as int[][]) == output
+			new S().cherryPickup(input as int[][]) == output
 
-        where:
+		where:
 
-            input                                                                                                                                                             || output
-//            [[0, 1, -1], [1, 0, -1], [1, 1, 1]]                                                                                                                               || 5
-//            [[1, 1, -1], [1, -1, 1], [-1, 1, 1]]                                                                                                                              || 0
-            [[1, 1, 1, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 1], [1, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 1, 1, 1]] || 15
+			input                                                                                                                                                             || output
+			[[0, 1, -1], [1, 0, -1], [1, 1, 1]]                                                                                                                               || 5
+			[[1, 1, -1], [1, -1, 1], [-1, 1, 1]]                                                                                                                              || 0
+			[[1, 1, 1, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 1], [1, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 1, 1, 1]] || 15
+			[[1, -1, -1, -1, -1], [1, 0, 1, -1, -1], [0, -1, 1, 0, 1], [1, 0, 1, 1, 0], [-1, -1, -1, 1, 1]]                                                                   || 10
 
-    }
+	}
 
 }
