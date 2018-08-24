f46680690799223c6bd5b170af2233c0857bbe0b
GD2LYAS <kirill.lyashko@partner.commerzsystems.com>
2018-08-24 17:27:06 +0200

    791. Custom Sort String

--- src/main/java/com/leetcode/problems/string/medium/CustomSortString.java
+++ src/main/java/com/leetcode/problems/string/medium/CustomSortString.java
@@ -3,26 +3,12 @@ package com.leetcode.problems.string.medium;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * https://leetcode.com/problems/custom-sort-string/description/
+ */
 public class CustomSortString {
 
-	public static void main(String[] args) {
-		int count = 0;
-		for (char ch = 'a'; ch <= 'z'; ch++) {
-			System.out.print((int) ch + ", ");
-			count++;
-		}
-
-		System.out.println();
-
-		for (char ch = 'a'; ch <= 'z'; ch++) {
-			System.out.print((ch - 97) + ", ");
-		}
-
-		System.out.println();
-		System.out.println(count);
-	}
-
-	class Solution {
+	class SolutionStream {
 		public String customSortString(String S, String T) {
 			int[] weights = new int[26];
 
@@ -38,6 +24,41 @@ public class CustomSortString {
 				.boxed()
 				.sorted(Comparator.comparingInt(ch -> weights[ch - 97]))
 				.forEach(i -> builder.append(Character.toChars(i)));
+
+			return builder.toString();
+		}
+	}
+
+	class Solution {
+		public String customSortString(String S, String T) {
+			boolean[] isInPattern = new boolean[26];
+			int[] counts = new int[26];
+
+			for (int i = 0; i < T.length(); i++) {
+				int idx = T.charAt(i) - 97;
+				counts[idx]++;
+			}
+
+			StringBuilder builder = new StringBuilder();
+
+			for (int i = 0; i < S.length(); i++) {
+				char ch = S.charAt(i);
+				int idx = ch - 97;
+				isInPattern[idx] = true;
+
+				for (int j = counts[idx]; j > 0; j--) {
+					builder.append(ch);
+				}
+			}
+
+			for (int i = 0; i < T.length(); i++) {
+				char ch = T.charAt(i);
+				int idx = ch - 97;
+				if (!isInPattern[idx]) {
+					builder.append(ch);
+				}
+				counts[idx]++;
+			}
 
 			return builder.toString();
 		}
