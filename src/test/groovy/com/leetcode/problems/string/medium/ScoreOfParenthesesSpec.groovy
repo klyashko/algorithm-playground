f8106151cf7aa4d52f65f5d21d79638a86f2e8af
GD2LYAS <kirill.lyashko@partner.commerzsystems.com>
2018-08-27 13:22:58 +0200

    856. Score of Parentheses

---
+++ src/test/groovy/com/leetcode/problems/string/medium/ScoreOfParenthesesSpec.groovy
@@ -0,0 +1,28 @@
+package com.leetcode.problems.string.medium
+
+import com.leetcode.problems.string.medium.ScoreOfParentheses.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class ScoreOfParenthesesSpec extends Specification {
+
+	@Unroll
+	def "scoreOfParentheses"() {
+
+		expect:
+
+			new S().scoreOfParentheses(str) == output
+
+		where:
+
+			str        || output
+			"()"       || 1
+			"(())"     || 2
+			"()()"     || 2
+			"(()(()))" || 6
+			"(())()"   || 3
+			"((()()))" || 8
+
+	}
+
+}
