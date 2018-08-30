ee8c87b7a0bc5a1acc9bd5b96e4fd81de42f651f
GD2LYAS <kirill.lyashko@partner.commerzsystems.com>
2018-08-30 16:05:56 +0200

    728. Self Dividing Numbers scratch

---
+++ src/main/java/com/leetcode/problems/math/easy/SelfDividingNumbers.java
@@ -0,0 +1,16 @@
+package com.leetcode.problems.math.easy;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/self-dividing-numbers/description/
+ */
+public class SelfDividingNumbers {
+
+	class Solution {
+		public List<Integer> selfDividingNumbers(int left, int right) {
+			return null;
+		}
+	}
+
+}
---
+++ src/test/groovy/com/leetcode/problems/math/easy/SelfDividingNumbersSpec.groovy
@@ -0,0 +1,23 @@
+package com.leetcode.problems.math.easy
+
+import com.leetcode.problems.math.easy.SelfDividingNumbers.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SelfDividingNumbersSpec extends Specification {
+
+	@Unroll
+	def "selfDividingNumbers"() {
+
+		expect:
+
+			new S().selfDividingNumbers(left, right) == output
+
+		where:
+
+			left || right || output
+			1    || 22    || [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+	}
+
+}
