c81a3904039099b8004ffb8a1439f77326cdc04d
kipill <kipill_90@mail.ru>
2018-09-05 20:13:37 +0200

    654. Maximum Binary Tree

--- src/main/java/com/leetcode/problems/tree/medium/MaximumBinaryTree.java
+++ src/main/java/com/leetcode/problems/tree/medium/MaximumBinaryTree.java
@@ -9,23 +9,21 @@ public class MaximumBinaryTree {
 
 	class Solution {
 		public TreeNode constructMaximumBinaryTree(int[] nums) {
-			return createNode(nums, 0, nums.length - 1);
+			return createNode(nums, 0, nums.length);
 		}
 
 		private TreeNode createNode(int[] nums, int left, int right) {
-			if (left > right) {
+			if (left == right) {
 				return null;
 			}
 			int idx = left;
-			int max = nums[left];
-			for (int i = left + 1; i <= right; i++) {
-				if (nums[i] > max) {
-					max = nums[i];
+			for (int i = left + 1; i < right; i++) {
+				if (nums[i] > nums[idx]) {
 					idx = i;
 				}
 			}
-			TreeNode node = new TreeNode(max);
-			node.left = createNode(nums, left, idx - 1);
+			TreeNode node = new TreeNode(nums[idx]);
+			node.left = createNode(nums, left, idx);
 			node.right = createNode(nums, idx + 1, right);
 			return node;
 		}
