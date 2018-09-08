74301a1165b3c4ef4dcb98693a7ab92733e05246
kipill <kipill_90@mail.ru>
2018-09-08 20:04:59 +0200

    76. Minimum Window Substring

--- src/test/groovy/com/leetcode/problems/hash/table/hard/MinimumWindowSubstringSpec.groovy
+++ src/test/groovy/com/leetcode/problems/hash/table/hard/MinimumWindowSubstringSpec.groovy
@@ -1,6 +1,7 @@
 package com.leetcode.problems.hash.table.hard
 
 import com.leetcode.problems.hash.table.hard.MinimumWindowSubstring.Solution as S
+import com.leetcode.problems.hash.table.hard.MinimumWindowSubstring.SolutionOptimized as SO
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -12,6 +13,23 @@ class MinimumWindowSubstringSpec extends Specification {
 		expect:
 
 			new S().minWindow(s, t) == output
+
+		where:
+
+			s               | t     || output
+			"ADOBECODEBANC" | "ABC" || "BANC"
+			"a"             | "aa"  || ""
+			"aa"            | "aa"  || "aa"
+			"a"             | "a"   || "a"
+
+	}
+
+	@Unroll
+	def "minWindow_opt"() {
+
+		expect:
+
+			new SO().minWindow(s, t) == output
 
 		where:
 
