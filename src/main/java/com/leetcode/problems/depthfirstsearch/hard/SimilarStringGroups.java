c40e75d20ae64d53d628d2bd860355c1d1618aeb
kipill <kipill_90@mail.ru>
2018-09-11 16:07:47 +0200

    839. Similar String Groups

--- src/main/java/com/leetcode/problems/depthfirstsearch/hard/SimilarStringGroups.java
+++ src/main/java/com/leetcode/problems/depthfirstsearch/hard/SimilarStringGroups.java
@@ -9,51 +9,50 @@ public class SimilarStringGroups {
 
 	class Solution {
 		public int numSimilarGroups(String[] A) {
-			Set<String> words = new HashSet<>(Arrays.asList(A));
+			Set<String> seen = new HashSet<>();
 			LinkedList<String> stack = new LinkedList<>();
 			int groups = 0;
 
-			while (!words.isEmpty()) {
-				String s = words.iterator().next();
-				words.remove(s);
+			for (String s : A) {
+				if (seen.contains(s)) {
+					continue;
+				}
 				stack.push(s);
+				seen.add(s);
 				groups++;
 
-				while (!stack.isEmpty() && !words.isEmpty()) {
+				while (!stack.isEmpty()) {
 					String current = stack.pop();
-					List<String> toRemove = new ArrayList<>();
-					for (String next : words) {
-						if (areSimilar(current, next)) {
+					for (String next : A) {
+						if (seen.contains(next)) {
+							continue;
+						}
+						if (test(current, next)) {
 							stack.push(next);
-							toRemove.add(next);
+							seen.add(next);
 						}
 					}
-					words.removeAll(toRemove);
 				}
 			}
 
 			return groups;
 		}
 
-		private boolean areSimilar(String s1, String s2) {
-			List<Integer> diff = difference(s1, s2);
-			if (diff.size() != 2) {
-				return false;
+		private boolean test(String s1, String s2) {
+			List<Integer> diff = new ArrayList<>();
+			for (int i = 0; i < s1.length(); i++) {
+				if (s1.charAt(i) != s2.charAt(i)) {
+					diff.add(i);
+				}
+				if (diff.size() > 2) {
+					return false;
+				}
 			}
 			int idx1 = diff.get(0);
 			int idx2 = diff.get(1);
 			return s1.charAt(idx1) == s2.charAt(idx2) && s1.charAt(idx2) == s2.charAt(idx1);
 		}
 
-		private List<Integer> difference(String s1, String s2) {
-			List<Integer> diff = new ArrayList<>();
-			for (int i = 0; i < s1.length(); i++) {
-				if (s1.charAt(i) != s2.charAt(i)) {
-					diff.add(i);
-				}
-			}
-			return diff;
-		}
 	}
 
 }
