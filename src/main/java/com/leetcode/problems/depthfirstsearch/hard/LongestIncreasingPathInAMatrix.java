465ef2a9fd8c505ee14ee44f75321842b2409418
kipill <kipill_90@mail.ru>
2018-09-12 10:52:14 +0200

    329. Longest Increasing Path in a Matrix

--- src/main/java/com/leetcode/problems/depthfirstsearch/hard/LongestIncreasingPathInAMatrix.java
+++ src/main/java/com/leetcode/problems/depthfirstsearch/hard/LongestIncreasingPathInAMatrix.java
@@ -16,36 +16,36 @@ public class LongestIncreasingPathInAMatrix {
 			int n = matrix.length;
 			int m = matrix[0].length;
 			cache = new int[n][m];
-			boolean[][] seen = new boolean[n][m];
 			int max = 0;
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < m; j++) {
-					max = Math.max(max, dfs(matrix, seen, i, j, 1));
+					max = Math.max(max, dfs(matrix, i, j));
 				}
 			}
 			return max;
 		}
 
-		private int dfs(int[][] matrix, boolean[][] seen, int r, int c, int len) {
+		private int dfs(int[][] matrix, int r, int c) {
 			if (cache[r][c] > 0) {
 				return cache[r][c];
 			}
+			int len = 1;
 			int max = len;
 			int n = matrix.length;
 			int m = matrix[0].length;
 			int curr = matrix[r][c];
 
 			if (r + 1 < n && matrix[r + 1][c] > curr) {
-				max = Math.max(max, dfs(matrix, seen, r + 1, c, len + 1));
+				max = Math.max(max, len + dfs(matrix, r + 1, c));
 			}
 			if (r - 1 >= 0 && matrix[r - 1][c] > curr) {
-				max = Math.max(max, dfs(matrix, seen, r - 1, c, len + 1));
+				max = Math.max(max, len + dfs(matrix, r - 1, c));
 			}
 			if (c + 1 < m && matrix[r][c + 1] > curr) {
-				max = Math.max(max, dfs(matrix, seen, r, c + 1, len + 1));
+				max = Math.max(max, len + dfs(matrix, r, c + 1));
 			}
 			if (c - 1 >= 0 && matrix[r][c - 1] > curr) {
-				max = Math.max(max, dfs(matrix, seen, r, c - 1, len + 1));
+				max = Math.max(max, len + dfs(matrix, r, c - 1));
 			}
 			cache[r][c] = max;
 			return max;
