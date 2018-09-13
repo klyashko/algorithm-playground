b28b0f7658d7c503bbb6dfbe9e1c4018143eaa3b
kipill <kipill_90@mail.ru>
2018-09-13 19:04:22 +0200

    844. Backspace String Compare

---
+++ src/main/java/com/leetcode/problems/twopointers/easy/BackspaceStringCompare.java
@@ -0,0 +1,31 @@
+package com.leetcode.problems.twopointers.easy;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/backspace-string-compare/description/
+ */
+public class BackspaceStringCompare {
+
+	class Solution {
+		public boolean backspaceCompare(String S, String T) {
+			return Arrays.equals(cleanUp(S), cleanUp(T));
+		}
+
+		private char[] cleanUp(String s) {
+			char[] chars = new char[s.length()];
+			int idx = 0;
+
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) == '#') {
+					idx = Math.max(0, idx - 1);
+				} else {
+					chars[idx++] = s.charAt(i);
+				}
+			}
+			return Arrays.copyOf(chars, idx);
+		}
+	}
+
+
+}
---
+++ src/test/groovy/com/leetcode/problems/twopointers/easy/BackspaceStringCompareSpec.groovy
@@ -0,0 +1,27 @@
+package com.leetcode.problems.twopointers.easy
+
+import com.leetcode.problems.twopointers.easy.BackspaceStringCompare.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class BackspaceStringCompareSpec extends Specification {
+
+	@Unroll
+	@SuppressWarnings("GroovyPointlessBoolean")
+	def "backspaceCompare"() {
+
+		expect:
+
+			new S().backspaceCompare(Str, T) == output
+
+		where:
+
+			Str    | T      || output
+			"ab#c" | "ad#c" || true
+			"ab##" | "c#d#" || true
+			"a##c" | "#a#c" || true
+			"a#c"  | "b"    || false
+
+	}
+
+}
