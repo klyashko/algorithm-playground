7dab68a6cb9b390db84d4b2deddd7e99fac25ff2
kipill <kipill_90@mail.ru>
2018-09-13 13:08:45 +0200

    363. Max Sum of Rectangle No Larger Than K

---
+++ src/test/groovy/com/leetcode/problems/binarysearch/hard/MaxSumOfRectangleNoLargerThanKSpec.groovy
@@ -0,0 +1,29 @@
+package com.leetcode.problems.binarysearch.hard
+
+import com.leetcode.problems.binarysearch.hard.MaxSumOfRectangleNoLargerThanK.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class MaxSumOfRectangleNoLargerThanKSpec extends Specification {
+
+	@Unroll
+	def "maxSumSubmatrix"() {
+
+		expect:
+
+			new S().maxSumSubmatrix(matrix as int[][], k) == output
+
+		where:
+
+			matrix       | k || output
+			[[1, 0, 1],
+			 [0, -2, 3]] | 2 || 2
+			[[1, 0, 1],
+			 [0, -2, 3]] | 5 || 4
+			[[1, 0, 1],
+			 [0, -2, 3]] | 3 || 3
+			[[2, 2, -1]] | 0 || -1
+
+	}
+
+}
