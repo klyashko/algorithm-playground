ee052603a3fdb0963281014f9c97eb2d0c03fe72
kipill <kipill_90@mail.ru>
2018-09-13 20:56:17 +0200

    828. Unique Letter String scratch

---
+++ src/main/java/com/leetcode/problems/twopointers/hard/UniqueLetterString.java
@@ -0,0 +1,14 @@
+package com.leetcode.problems.twopointers.hard;
+
+/**
+ * https://leetcode.com/problems/unique-letter-string/description/
+ */
+public class UniqueLetterString {
+
+	class Solution {
+		public int uniqueLetterString(String S) {
+			return 0;
+		}
+	}
+
+}
