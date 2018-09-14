8e11de650501123b252485757050a08f09a1a85e
kipill <kipill_90@mail.ru>
2018-09-14 19:54:49 +0200

    828. Unique Letter String

--- src/main/java/com/leetcode/problems/twopointers/hard/UniqueLetterString.java
+++ src/main/java/com/leetcode/problems/twopointers/hard/UniqueLetterString.java
@@ -1,5 +1,10 @@
 package com.leetcode.problems.twopointers.hard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/unique-letter-string/description/
  */
@@ -7,7 +12,25 @@ public class UniqueLetterString {
 
 	class Solution {
 		public int uniqueLetterString(String S) {
-			return 0;
+			Map<Character, List<Integer>> index = new HashMap<>();
+			for (int i = 0; i < S.length(); ++i) {
+				char c = S.charAt(i);
+				if (!index.containsKey(c)) {
+					index.put(c, new ArrayList<>());
+				}
+				index.get(c).add(i);
+			}
+
+			long ans = 0;
+			for (List<Integer> A : index.values()) {
+				for (int i = 0; i < A.size(); ++i) {
+					long prev = i > 0 ? A.get(i - 1) : -1;
+					long next = i < A.size() - 1 ? A.get(i + 1) : S.length();
+					ans += (A.get(i) - prev) * (next - A.get(i));
+				}
+			}
+
+			return (int) ans % 1_000_000_007;
 		}
 	}
 
