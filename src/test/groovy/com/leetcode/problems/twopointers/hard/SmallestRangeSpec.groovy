ddd1a0acb77cdc02af8fcf3be6ebf9f960b2a025
kipill <kipill_90@mail.ru>
2018-09-15 11:15:03 +0200

    632. Smallest Range

---
+++ src/test/groovy/com/leetcode/problems/twopointers/hard/SmallestRangeSpec.groovy
@@ -0,0 +1,32 @@
+package com.leetcode.problems.twopointers.hard
+
+import com.leetcode.problems.twopointers.hard.SmallestRange.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SmallestRangeSpec extends Specification {
+
+	@Unroll
+	def "smallestRange"() {
+
+		expect:
+
+			new S().smallestRange(nums) == output as int[]
+
+		where:
+
+			nums               || output
+			[[4, 10, 15, 24, 26],
+			 [0, 9, 12, 20],
+			 [5, 18, 22, 30]]  || [20, 24]
+			[[1, 2, 3],
+			 [1, 2, 3],
+			 [1, 2, 3]]        || [1, 1]
+			[[1, 3, 5, 7, 9],
+			 [2, 4, 6, 8, 10]] || [1, 2]
+			[[-5, -4, -3, -2, -1],
+			 [1, 2, 3, 4, 5]]  || [-1, 1]
+
+	}
+
+}
