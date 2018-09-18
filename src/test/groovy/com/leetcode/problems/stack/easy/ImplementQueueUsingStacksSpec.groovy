63796e66d22027f956cdb9f504dd319cea799cfd
kipill <kipill_90@mail.ru>
2018-09-18 19:01:11 +0200

    232. Implement Queue using Stacks

---
+++ src/test/groovy/com/leetcode/problems/stack/easy/ImplementQueueUsingStacksSpec.groovy
@@ -0,0 +1,27 @@
+package com.leetcode.problems.stack.easy
+
+import com.leetcode.problems.stack.easy.ImplementQueueUsingStacks.MyQueue as MQ
+import spock.lang.Specification
+
+class ImplementQueueUsingStacksSpec extends Specification {
+
+	def "myQueue"() {
+
+		given:
+
+			def queue = new MQ()
+
+			queue.push(1)
+			queue.push(2)
+
+		expect:
+
+			queue.peek() == 1
+			queue.pop() == 1
+			!queue.empty()
+			queue.pop() == 2
+			queue.empty()
+
+	}
+
+}
