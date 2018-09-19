c594c1796ef6230e8520799ef1ceda1e53084ccd
kipill <kipill_90@mail.ru>
2018-09-19 16:35:17 +0200

    526. Beautiful Arrangement

--- src/main/java/com/leetcode/problems/backtracking/medium/BeautifulArrangement.java
+++ src/main/java/com/leetcode/problems/backtracking/medium/BeautifulArrangement.java
@@ -7,20 +7,18 @@ public class BeautifulArrangement {
 
 	class Solution {
 		public int countArrangement(int N) {
-			return backtracking(new int[N + 1], 1, N, new boolean[N + 1]);
+			return backtracking(1, N, new boolean[N + 1]);
 		}
 
-		private int backtracking(int[] size, int idx, int N, boolean[] used) {
-			if (idx == size.length) {
-//				System.out.println(Arrays.toString(size));
+		private int backtracking(int idx, int N, boolean[] used) {
+			if (idx == N + 1) {
 				return 1;
 			}
 			int count = 0;
 			for (int i = 1; i <= N; i++) {
 				if (!used[i] && (i % idx == 0 || idx % i == 0)) {
-					size[idx] = i;
 					used[i] = true;
-					count += backtracking(size, idx + 1, N, used);
+					count += backtracking(idx + 1, N, used);
 					used[i] = false;
 				}
 			}
