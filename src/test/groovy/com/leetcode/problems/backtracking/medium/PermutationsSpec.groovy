9010ce599944976e9482e388ce3bea6a43f63870
kipill <kipill_90@mail.ru>
2018-09-19 16:58:04 +0200

    46. Permutations

---
+++ src/test/groovy/com/leetcode/problems/backtracking/medium/PermutationsSpec.groovy
@@ -0,0 +1,28 @@
+package com.leetcode.problems.backtracking.medium
+
+import com.leetcode.problems.backtracking.medium.Permutations.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class PermutationsSpec extends Specification {
+
+	@Unroll
+	def "permute"() {
+
+		expect:
+
+			new S().permute(nums as int[]) as Set == output as Set
+
+		where:
+
+			nums      || output
+			[1, 2, 3] || [[1, 2, 3],
+						  [1, 3, 2],
+						  [2, 1, 3],
+						  [2, 3, 1],
+						  [3, 1, 2],
+						  [3, 2, 1]]
+
+	}
+
+}
