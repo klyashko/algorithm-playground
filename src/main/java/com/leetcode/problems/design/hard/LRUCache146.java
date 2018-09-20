ab57383bf54396925f13930480fb766a78f17d5f
kipill <kipill_90@mail.ru>
2018-09-20 19:05:20 +0200

    146. LRU Cache

---
+++ src/main/java/com/leetcode/problems/design/hard/LRUCache146.java
@@ -0,0 +1,69 @@
+package com.leetcode.problems.design.hard;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
+/**
+ * https://leetcode.com/problems/lru-cache/description/
+ */
+public class LRUCache146 {
+
+	public static LRUCache lruCache(int capacity) {
+		return new LRUCache146().createLRUCache(capacity);
+	}
+
+	private LRUCache createLRUCache(int capacity) {
+		return new LRUCache(capacity);
+	}
+
+	class LRUCache {
+
+		private Map<Integer, Entry> map = new HashMap<>();
+		private TreeSet<Entry> set = new TreeSet<>(Comparator.comparingLong(e -> e.time));
+		private long time;
+		private int capacity;
+
+		public LRUCache(int capacity) {
+			this.capacity = capacity;
+		}
+
+		public int get(int key) {
+			Entry entry = map.get(key);
+			if (entry == null) {
+				return -1;
+			}
+			set.remove(entry);
+			entry.time = time++;
+			set.add(entry);
+			return entry.val;
+		}
+
+		public void put(int key, int value) {
+			if (map.size() == capacity && !map.containsKey(key)) {
+				Entry first = set.first();
+				set.remove(first);
+				map.remove(first.key);
+			} else if (map.containsKey(key)) {
+				set.remove(map.get(key));
+			}
+			Entry entry = new Entry(key, value, time++);
+			map.put(entry.key, entry);
+			set.add(entry);
+		}
+
+		private class Entry {
+			Integer key, val;
+			long time;
+
+			public Entry(Integer key, Integer val, long time) {
+				this.key = key;
+				this.val = val;
+				this.time = time;
+			}
+		}
+
+	}
+
+}
---
+++ src/test/groovy/com/leetcode/problems/design/hard/LRUCache146Spec.groovy
@@ -0,0 +1,34 @@
+package com.leetcode.problems.design.hard
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.leetcode.problems.design.hard.LRUCache146.lruCache as cache
+
+class LRUCache146Spec extends Specification {
+
+	@Unroll
+	@SuppressWarnings("GroovyAssignabilityCheck")
+	def "LRUCache"() {
+
+		given:
+
+			def cache = cache(capacity)
+
+		expect:
+
+			[methods, args, results].transpose().each {
+				method, arg, result ->
+					println("$method with ($arg) expect $result")
+					assert cache."$method"(arg) == result
+			}
+
+		where:
+
+			capacity | methods                                                         | args                                                      || results
+			2        | ["put", "put", "get", "put", "get", "put", "get", "get", "get"] | [[1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]] || [null, null, 1, null, -1, null, -1, 3, 4]
+			2        | ["put", "put", "put", "put", "get", "get"]                      | [[2, 1], [1, 1], [2, 3], [4, 1], [1], [2]]                || [null, null, null, null, -1, 3]
+
+	}
+
+}
