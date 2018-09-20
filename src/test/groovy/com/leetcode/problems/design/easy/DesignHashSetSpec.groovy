e3a787c45a73d0ad7c2b564b9ecfe8c3c1120be4
kipill <kipill_90@mail.ru>
2018-09-20 13:05:30 +0200

    705. Design HashSet

---
+++ src/test/groovy/com/leetcode/problems/design/easy/DesignHashSetSpec.groovy
@@ -0,0 +1,30 @@
+package com.leetcode.problems.design.easy
+
+import com.leetcode.problems.design.easy.DesignHashSet.MyHashSet as HS
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class DesignHashSetSpec extends Specification {
+
+	@Unroll
+	def "myHashSet"() {
+
+		given:
+
+			def hs = new HS()
+
+		expect:
+
+			[methods, args, results].transpose().each {
+				method, arg, result ->
+					println("$method with ($arg) expect $result")
+					assert hs."$method"(arg) == result
+			}
+
+		where:
+
+			methods                                                                         | args                                     || results
+			["add", "add", "contains", "contains", "add", "contains", "remove", "contains"] | [[1], [2], [1], [3], [2], [2], [2], [2]] || [null, null, true, false, null, true, null, false]
+	}
+
+}
