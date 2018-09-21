685a969138fbecb73836afeb2669ed8c9c561aa7
kipill <kipill_90@mail.ru>
2018-09-21 18:56:46 +0200

    406. Queue Reconstruction by Height

---
+++ src/test/groovy/com/leetcode/problems/greedy/medium/QueueReconstructionByHeightSpec.groovy
@@ -0,0 +1,23 @@
+package com.leetcode.problems.greedy.medium
+
+import com.leetcode.problems.greedy.medium.QueueReconstructionByHeight.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class QueueReconstructionByHeightSpec extends Specification {
+
+	@Unroll
+	def "reconstructQueue"() {
+
+		expect:
+
+			new S().reconstructQueue(people as int[][]) == output as int[][]
+
+		where:
+
+			people                                           || output
+			[[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]] || [[5, 0], [7, 0], [5, 2], [6, 1], [4, 4], [7, 1]]
+
+	}
+
+}
