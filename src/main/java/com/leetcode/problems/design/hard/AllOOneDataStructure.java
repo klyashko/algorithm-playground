7ea7f42aa16d015ee07012a25b44eff2b4e91727
kipill <kipill_90@mail.ru>
2018-09-21 16:16:00 +0200

    432. All O`one Data Structure

---
+++ src/main/java/com/leetcode/problems/design/hard/AllOOneDataStructure.java
@@ -0,0 +1,104 @@
+package com.leetcode.problems.design.hard;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/all-oone-data-structure/description/
+ */
+public class AllOOneDataStructure {
+
+	class AllOne {
+
+		private Map<String, Integer> counts = new HashMap<>();
+		private Map<Integer, Set<String>> strings = new HashMap<>();
+		private int min, max;
+
+		/**
+		 * Initialize your data structure here.
+		 */
+		public AllOne() { }
+
+		/**
+		 * Inserts a new key <Key> with value 1. Or increments an existing key by 1.
+		 */
+		public void inc(String key) {
+			int curr = getCountForUpdate(key);
+			/** Update min */
+			if (curr == min && strings.getOrDefault(curr, Collections.emptySet()).isEmpty()) {
+				min++;
+			}
+			curr++;
+			updateCount(key, curr);
+			/** Update max */
+			min = Math.min(min, curr);
+			max = Math.max(max, curr);
+		}
+
+		/**
+		 * Decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
+		 */
+		public void dec(String key) {
+			int curr = getCountForUpdate(key);
+			if (curr == 0) {
+				return;
+			}
+			if (curr == max && strings.getOrDefault(curr, Collections.emptySet()).isEmpty()) {
+				max--;
+			}
+			curr--;
+			updateCount(key, curr);
+			min = Math.min(min, curr);
+			if (min == 0 && !strings.isEmpty()) {
+				while (strings.get(min) == null) {
+					min++;
+				}
+			}
+		}
+
+		/**
+		 * Returns one of the keys with maximal value.
+		 */
+		public String getMaxKey() {
+			return getAnyForKey(max);
+		}
+
+		/**
+		 * Returns one of the keys with Minimal value.
+		 */
+		public String getMinKey() {
+			return getAnyForKey(min);
+		}
+
+		private int getCountForUpdate(String key) {
+			int curr = counts.getOrDefault(key, 0);
+			if (strings.containsKey(curr)) {
+				Set<String> set = strings.get(curr);
+				set.remove(key);
+				if (set.isEmpty()) {
+					strings.remove(curr);
+				}
+			}
+			return curr;
+		}
+
+		private void updateCount(String key, int count) {
+			if (count == 0) {
+				counts.remove(key);
+			} else {
+				counts.put(key, count);
+				if (!strings.containsKey(count)) {
+					strings.put(count, new HashSet<>());
+				}
+				strings.get(count).add(key);
+			}
+		}
+
+		private String getAnyForKey(int key) {
+			if (key == 0) {
+				return "";
+			}
+			return strings.get(key).iterator().next();
+		}
+	}
+
+}
