bdc5c0f4ee9c3106dc3e5f8edabe3e834d7d3198
kipill <kipill_90@mail.ru>
2018-09-21 20:16:31 +0200

    452. Minimum Number of Arrows to Burst Balloons

--- src/main/java/com/leetcode/problems/greedy/medium/MinimumNumberOfArrowsToBurstBalloons.java
+++ src/main/java/com/leetcode/problems/greedy/medium/MinimumNumberOfArrowsToBurstBalloons.java
@@ -1,6 +1,7 @@
 package com.leetcode.problems.greedy.medium;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
@@ -9,15 +10,13 @@ public class MinimumNumberOfArrowsToBurstBalloons {
 
 	class Solution {
 		public int findMinArrowShots(int[][] points) {
-			Arrays.sort(points, (p1, p2) -> p1[0] == p2[0] ? p1[1] - p2[1] : p1[0] - p2[0]);
-			int count = 0, idx = 0;
-			while (idx < points.length) {
-				int[] curr = points[idx++];
-				while (idx < points.length && points[idx][0] <= curr[1]) {
-					curr[1] = Math.min(curr[1], points[idx][1]);
-					idx++;
+			Arrays.sort(points, Comparator.comparingInt(p -> p[0]));
+			int count = 0;
+			for (int idx = 0; idx < points.length; count++) {
+				int limit = points[idx++][1];
+				while (idx < points.length && points[idx][0] <= limit) {
+					limit = Math.min(limit, points[idx++][1]);
 				}
-				count++;
 			}
 			return count;
 		}
