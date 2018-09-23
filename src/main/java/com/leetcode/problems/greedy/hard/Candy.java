55ca688cdc2c19a3ad616d0ec0cb8cfaead8d60a
kipill <kipill_90@mail.ru>
2018-09-23 17:05:59 +0200

    135. Candy

--- src/main/java/com/leetcode/problems/greedy/hard/Candy.java
+++ src/main/java/com/leetcode/problems/greedy/hard/Candy.java
@@ -7,27 +7,21 @@ public class Candy {
 
 	class Solution {
 		public int candy(int[] ratings) {
-			int curr = 1;
 			int[] candies = new int[ratings.length];
 			candies[0] = 1;
 			for (int i = 1; i < ratings.length; i++) {
 				if (ratings[i] > ratings[i - 1]) {
-					curr++;
+					candies[i] = candies[i - 1] + 1;
 				} else if (ratings[i] == ratings[i - 1]) {
-					curr = 1;
+					candies[i] = 1;
 				} else {
-					curr = 1;
-					candies[i] = curr;
+					candies[i] = 1;
 					for (int j = i - 1; j >= 0 && ratings[j] > ratings[j + 1] && candies[j] <= candies[j + 1]; j--) {
-						curr++;
-						candies[j] = curr;
+						candies[j] = candies[j + 1] + 1;
 					}
-					curr = 1;
 				}
-				candies[i] = curr;
 			}
 
-//			System.out.println(Arrays.toString(candies));
 			int count = 0;
 			for (int c : candies) {
 				count += c;
