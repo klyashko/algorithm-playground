293e58f8427ef21b872150877e5685071ce20509
kipill <kipill_90@mail.ru>
2018-09-23 20:04:51 +0200

    630. Course Schedule III

--- src/main/java/com/leetcode/problems/greedy/hard/CourseScheduleIII.java
+++ src/main/java/com/leetcode/problems/greedy/hard/CourseScheduleIII.java
@@ -2,6 +2,7 @@ package com.leetcode.problems.greedy.hard;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * https://leetcode.com/problems/course-schedule-iii/description/
@@ -10,33 +11,21 @@ public class CourseScheduleIII {
 
 	class Solution {
 		public int scheduleCourse(int[][] courses) {
-			if (courses.length == 0) {
-				return 0;
-			}
 			Arrays.sort(courses, Comparator.comparingInt(c -> c[1]));
+			PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+			int time = 0;
 			for (int[] c : courses) {
-				System.out.println(Arrays.toString(c));
+				if (time + c[0] <= c[1]) {
+					queue.offer(c[0]);
+					time += c[0];
+				} else //noinspection ConstantConditions
+					if (!queue.isEmpty() && queue.peek() > c[0]) {
+						//noinspection ConstantConditions
+						time += c[0] - queue.poll();
+						queue.offer(c[0]);
+					}
 			}
-			return dfs(courses, 0, courses[0][0], new int[courses.length]);
-		}
-
-		private int dfs(int[][] courses, int curr, int day, int[] cache) {
-			if (curr == courses.length) {
-				return 0;
-			} else if (cache[curr] > 0) {
-				return cache[curr];
-			}
-			int max = 1;
-//			for (int i = curr + 1; i < courses.length - max + 1; i++) {
-			for (int i = curr + 1; i < courses.length; i++) {
-				int[] course = courses[i];
-				int finish = day + course[0];
-				if (finish <= course[1]) {
-					max = Math.max(max, dfs(courses, i, finish, cache) + 1);
-				}
-			}
-			cache[curr] = max;
-			return max;
+			return queue.size();
 		}
 	}
 
