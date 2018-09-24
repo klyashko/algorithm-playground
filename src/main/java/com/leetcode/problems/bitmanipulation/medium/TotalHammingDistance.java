d6b9ccff624680834fc8bf2d0c3bcf0062c38294
kipill <kipill_90@mail.ru>
2018-09-24 19:45:49 +0200

    477. Total Hamming Distance

---
+++ src/main/java/com/leetcode/problems/bitmanipulation/medium/TotalHammingDistance.java
@@ -0,0 +1,24 @@
+package com.leetcode.problems.bitmanipulation.medium;
+
+/**
+ * https://leetcode.com/problems/total-hamming-distance/description/
+ */
+public class TotalHammingDistance {
+
+	class Solution {
+		public int totalHammingDistance(int[] nums) {
+			int dist = 0;
+			for (int i = 0; i < nums.length; i++) {
+				for (int j = i + 1; j < nums.length; j++) {
+					dist += distance(nums[i], nums[j]);
+				}
+			}
+			return dist;
+		}
+
+		private int distance(int x, int y) {
+			return Integer.bitCount(x ^ y);
+		}
+	}
+
+}
---
+++ src/test/groovy/com/leetcode/problems/bitmanipulation/medium/TotalHammingDistanceSpec.groovy
@@ -0,0 +1,23 @@
+package com.leetcode.problems.bitmanipulation.medium
+
+import com.leetcode.problems.bitmanipulation.medium.TotalHammingDistance.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class TotalHammingDistanceSpec extends Specification {
+
+	@Unroll
+	def "totalHammingDistance"() {
+
+		expect:
+
+			new S().totalHammingDistance(nums as int[]) == output
+
+		where:
+
+			nums       || output
+			[4, 14, 2] || 6
+
+	}
+
+}
