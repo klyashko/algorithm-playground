42c7c6aa1bb0540c4a20afd1bd5a019c0a54b65d
kipill <kipill_90@mail.ru>
2018-09-24 19:39:35 +0200

    421. Maximum XOR of Two Numbers in an Array

---
+++ src/main/java/com/leetcode/problems/bitmanipulation/medium/MaximumXOROfTwoNumbersInAnArray.java
@@ -0,0 +1,32 @@
+package com.leetcode.problems.bitmanipulation.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/description/
+ */
+public class MaximumXOROfTwoNumbersInAnArray {
+
+	class Solution {
+		public int findMaximumXOR(int[] nums) {
+			int max = 0, mask = 0;
+			for (int i = 31; i >= 0; i--) {
+				mask = mask | (1 << i);
+				Set<Integer> set = new HashSet<>();
+				for (int num : nums) {
+					set.add(num & mask);
+				}
+				int tmp = max | (1 << i);
+				for (int prefix : set) {
+					if (set.contains(tmp ^ prefix)) {
+						max = tmp;
+						break;
+					}
+				}
+			}
+			return max;
+		}
+	}
+
+}
---
+++ src/test/groovy/com/leetcode/problems/bitmanipulation/medium/MaximumXOROfTwoNumbersInAnArraySpec.groovy
@@ -0,0 +1,23 @@
+package com.leetcode.problems.bitmanipulation.medium
+
+import com.leetcode.problems.bitmanipulation.medium.MaximumXOROfTwoNumbersInAnArray.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class MaximumXOROfTwoNumbersInAnArraySpec extends Specification {
+
+	@Unroll
+	def "findMaximumXOR"() {
+
+		expect:
+
+			new S().findMaximumXOR(nums as int[]) == output
+
+		where:
+
+			nums                 || output
+			[3, 10, 5, 25, 2, 8] || 28
+
+	}
+
+}
