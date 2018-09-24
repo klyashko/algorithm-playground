ca7157810f1f8c13565347ad28bf0f4e764de14f
kipill <kipill_90@mail.ru>
2018-09-24 14:17:08 +0200

    21. Merge Two Sorted Lists

---
+++ src/main/java/com/leetcode/problems/linkedlist/easy/MergeTwoSortedLists.java
@@ -0,0 +1,48 @@
+package com.leetcode.problems.linkedlist.easy;
+
+import com.leetcode.problems.linkedlist.ListNode;
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ */
+public class MergeTwoSortedLists {
+
+	class Solution {
+		public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+			ListNode curr;
+			if (l1 != null && l2 != null) {
+				if (l1.val < l2.val) {
+					curr = l1;
+					l1 = l1.next;
+				} else {
+					curr = l2;
+					l2 = l2.next;
+				}
+			} else if (l1 != null) {
+				curr = l1;
+				l1 = l1.next;
+			} else {
+				curr = l2;
+				l2 = l2 != null ? l2.next : null;
+			}
+			ListNode head = curr;
+			while (l1 != null && l2 != null) {
+				if (l1.val < l2.val) {
+					curr.next = l1;
+					l1 = l1.next;
+				} else {
+					curr.next = l2;
+					l2 = l2.next;
+				}
+				curr = curr.next;
+			}
+			if (l1 != null) {
+				curr.next = l1;
+			} else if (l2 != null) {
+				curr.next = l2;
+			}
+			return head;
+		}
+	}
+
+}
