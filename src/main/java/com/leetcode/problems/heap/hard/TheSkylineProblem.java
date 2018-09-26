5f12f28542d39b1e2a6bc2cf0b97eb3d46abbb9b
kipill <kipill_90@mail.ru>
2018-09-26 19:39:00 +0200

    218. The Skyline Problem

--- src/main/java/com/leetcode/problems/heap/hard/TheSkylineProblem.java
+++ src/main/java/com/leetcode/problems/heap/hard/TheSkylineProblem.java
@@ -92,4 +92,79 @@ public class TheSkylineProblem {
 		}
 	}
 
+	class Solution_ {
+		public List<int[]> getSkyline(int[][] buildings) {
+			LinkedList<Node> list = new LinkedList<>();
+			list.add(new Node(0, Integer.MAX_VALUE, 0));
+//			System.out.println(list);
+			for (int[] b : buildings) {
+				insert(list, b);
+//				System.out.println(list);
+			}
+
+			List<int[]> ans = new ArrayList<>();
+			int height = 0;
+			for (Node curr : list) {
+				if (curr.height > height) {
+					ans.add(new int[]{curr.left, curr.height});
+					height = curr.height;
+				} else if (curr.height < height) {
+					ans.add(new int[]{curr.left - 1, curr.height});
+					height = curr.height;
+				}
+
+			}
+			if (list.getLast().height > 0) {
+				ans.add(new int[]{Integer.MAX_VALUE, 0});
+			}
+			return ans;
+		}
+
+		private void insert(LinkedList<Node> list, int[] building) {
+			int li = building[0], ri = building[1], hi = building[2];
+			ListIterator<Node> iterator = list.listIterator();
+			while (iterator.hasNext()) {
+				Node curr = iterator.next();
+				if (curr.right < li) {
+					continue;
+				} else if (curr.left > ri) {
+					break;
+				} else {
+					if (curr.height < hi) {
+						int left = li - curr.left;
+						int right = curr.right - ri;
+						if (left > 0) {
+							iterator.set(new Node(curr.left, li - 1, curr.height));
+							iterator.add(new Node(li, Math.min(ri, curr.right), hi));
+						} else {
+							iterator.set(new Node(li - left, Math.min(ri, curr.right), hi));
+						}
+						if (right > 0) {
+							iterator.add(new Node(ri + 1, curr.right, curr.height));
+						}
+					}
+				}
+			}
+		}
+
+		private class Node {
+			int left, right, height;
+
+			public Node(int left, int right, int height) {
+				this.left = left;
+				this.right = right;
+				this.height = height;
+			}
+
+			@Override
+			public String toString() {
+				return "Node{" +
+						"left=" + left +
+						", right=" + right +
+						", height=" + height +
+						'}';
+			}
+		}
+	}
+
 }
