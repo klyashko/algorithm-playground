c7f65b146d7c249b02178da982c66638085202e8
kipill <kipill_90@mail.ru>
2018-09-28 16:56:58 +0200

    802. Find Eventual Safe States

--- src/main/java/com/leetcode/problems/graph/medium/FindEventualSafeStates.java
+++ src/main/java/com/leetcode/problems/graph/medium/FindEventualSafeStates.java
@@ -12,13 +12,10 @@ public class FindEventualSafeStates {
 		public List<Integer> eventualSafeNodes(int[][] graph) {
 			List<Integer> result = new ArrayList<>();
 			boolean[] cache = new boolean[graph.length];
+			int[] colors = new int[graph.length];
 			for (int i = graph.length - 1; i >= 0; i--) {
-				dfs(graph, new int[graph.length], cache, i);
-			}
-
-			for (int i = 0; i < cache.length; i++) {
-				if (cache[i]) {
-					result.add(i);
+				if (dfs(graph, colors, cache, i)) {
+					result.add(0, i);
 				}
 			}
 
@@ -26,18 +23,15 @@ public class FindEventualSafeStates {
 		}
 
 		private boolean dfs(int[][] edges, int[] colors, boolean[] cache, int curr) {
-			if (edges[curr].length == 0 || cache[curr]) {
-				cache[curr] = true;
+			if (cache[curr] || colors[curr] == 2) {
 				return true;
+			} else if (colors[curr] == 1) {
+				return false;
 			}
 
 			colors[curr] = 1;
 			for (int e : edges[curr]) {
-				if (colors[e] == 0) {
-					if (!dfs(edges, colors, cache, e)) {
-						return false;
-					}
-				} else if (colors[e] == 1) {
+				if (colors[e] == 1 || !dfs(edges, colors, cache, e)) {
 					return false;
 				}
 			}
