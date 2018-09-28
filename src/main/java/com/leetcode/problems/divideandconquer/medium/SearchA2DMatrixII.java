1c4f57679b1340c3f24d3125ebefb9f6ac416ab4
kipill <kipill_90@mail.ru>
2018-09-28 20:38:36 +0200

    240. Search a 2D Matrix II

---
+++ src/main/java/com/leetcode/problems/divideandconquer/medium/SearchA2DMatrixII.java
@@ -0,0 +1,32 @@
+package com.leetcode.problems.divideandconquer.medium;
+
+/**
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ */
+public class SearchA2DMatrixII {
+
+	class Solution {
+		public boolean searchMatrix(int[][] matrix, int target) {
+			for (int[] row : matrix) {
+				if (binarySearch(row, target)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private boolean binarySearch(int[] array, int target) {
+			int li = 0, ri = array.length;
+			while (li < ri) {
+				int mid = li + (ri - li) / 2;
+				if (array[mid] < target) {
+					li = mid + 1;
+				} else {
+					ri = mid;
+				}
+			}
+			return li < array.length && array[li] == target;
+		}
+	}
+
+}
---
+++ src/test/groovy/com/leetcode/problems/divideandconquer/medium/SearchA2DMatrixIISpec.groovy
@@ -0,0 +1,34 @@
+package com.leetcode.problems.divideandconquer.medium
+
+import com.leetcode.problems.divideandconquer.medium.SearchA2DMatrixII.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SearchA2DMatrixIISpec extends Specification {
+
+	@Unroll
+	@SuppressWarnings("GroovyPointlessBoolean")
+	def "searchMatrix"() {
+
+		expect:
+
+			new S().searchMatrix(matrix as int[][], target) == output
+
+		where:
+
+			matrix                 | target || output
+			[[1, 4, 7, 11, 15],
+			 [2, 5, 8, 12, 19],
+			 [3, 6, 9, 16, 22],
+			 [10, 13, 14, 17, 24],
+			 [18, 21, 23, 26, 30]] | 5      || true
+			[[1, 4, 7, 11, 15],
+			 [2, 5, 8, 12, 19],
+			 [3, 6, 9, 16, 22],
+			 [10, 13, 14, 17, 24],
+			 [18, 21, 23, 26, 30]] | 20     || false
+			[]                     | 1      || false
+
+	}
+
+}
