79be51607de63cf1e746e11025505973c0a7d975
kipill <kipill_90@mail.ru>
2018-09-29 15:59:39 +0200

    493. Reverse Pairs scratch

--- src/main/java/com/leetcode/problems/divideandconquer/hard/ReversePairs.java
+++ src/main/java/com/leetcode/problems/divideandconquer/hard/ReversePairs.java
@@ -1,8 +1,6 @@
 package com.leetcode.problems.divideandconquer.hard;
 
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/reverse-pairs/description/
@@ -11,11 +9,11 @@ public class ReversePairs {
 
 	class Solution {
 		public int reversePairs(int[] nums) {
-			TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<>();
+			TreeMap<Integer, List<Integer>> map = new TreeMap<>();
 			for (int i = 0; i < nums.length; i++) {
 				int num = nums[i];
 				if (!map.containsKey(num)) {
-					map.put(num, new TreeSet<>());
+					map.put(num, new ArrayList<>());
 				}
 				map.get(num).add(i);
 			}
@@ -23,9 +21,14 @@ public class ReversePairs {
 			for (int i = 0; i < nums.length; i++) {
 				int num = nums[i];
 				int limit = num % 2 == 1 ? num / 2 : num / 2 - 1;
-				SortedMap<Integer, TreeSet<Integer>> tmp = map.headMap(limit, true);
+				SortedMap<Integer, List<Integer>> tmp = map.headMap(limit, true);
 				for (Integer key : tmp.keySet()) {
-					count += tmp.get(key).tailSet(i, false).size();
+					List<Integer> list = tmp.get(key);
+					int idx = Collections.binarySearch(list, i + 1);
+					if (idx < 0) {
+						idx = -idx - 1;
+					}
+					count += list.size() - idx;
 				}
 			}
 			return count;
