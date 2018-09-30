edbaa1bd0b7e393000ed2af9269f6f2923db9b89
kipill <kipill_90@mail.ru>
2018-09-30 12:46:53 +0200

    282. Expression Add Operators

--- src/main/java/com/leetcode/problems/divideandconquer/hard/ExpressionAddOperators.java
+++ src/main/java/com/leetcode/problems/divideandconquer/hard/ExpressionAddOperators.java
@@ -11,25 +11,14 @@ public class ExpressionAddOperators {
 	class Solution {
 		public List<String> addOperators(String num, int target) {
 			List<String> result = new ArrayList<>();
-			if (num == null || num.isEmpty()) {
-				return result;
-			}
-			int offset = Long.valueOf(num.substring(0, Math.min(10, num.length()))) <= Integer.MAX_VALUE ? 10 : 9;
-			int until = num.charAt(0) == '0' ? 1 : Math.min(offset, num.length());
-			StringBuilder builder = new StringBuilder();
-			for (int i = 0; i < until; i++) {
-				Integer n = Integer.valueOf(num.substring(0, i + 1));
-				builder.append(n);
-				backtracking(result, num, builder, i + 1, '+', n, n, target);
-				builder.setLength(0);
-			}
+			backtracking(result, num, new StringBuilder(), 0, '*', 0, 0, target);
 			return result;
 		}
 
 		private void backtracking(List<String> result, String nums, StringBuilder expression, int idx, char op, int num, int val, int target) {
 			if (idx == nums.length()) {
 				if (val == target) {
-					result.add(expression.toString());
+					result.add(expression.substring(1));
 				}
 				return;
 			}
@@ -43,24 +32,26 @@ public class ExpressionAddOperators {
 				backtracking(result, nums, expression, i + 1, '+', n, val + n, target);
 				expression.setLength(len);
 
-				/** - */
-				expression.append("-").append(n);
-				backtracking(result, nums, expression, i + 1, '-', n, val - n, target);
-				expression.setLength(len);
+				if (idx != 0) {
+					/** - */
+					expression.append("-").append(n);
+					backtracking(result, nums, expression, i + 1, '-', n, val - n, target);
+					expression.setLength(len);
 
-				/** * */
-				expression.append("*").append(n);
-				int tmp = val;
-				if (op == '+') {
-					tmp -= num;
-					tmp += num * n;
-					backtracking(result, nums, expression, i + 1, '+', num * n, tmp, target);
-				} else if (op == '-') {
-					tmp += num;
-					tmp -= num * n;
-					backtracking(result, nums, expression, i + 1, '-', num * n, tmp, target);
+					/** * */
+					expression.append("*").append(n);
+					int tmp = val;
+					if (op == '+') {
+						tmp -= num;
+						tmp += num * n;
+						backtracking(result, nums, expression, i + 1, '+', num * n, tmp, target);
+					} else if (op == '-') {
+						tmp += num;
+						tmp -= num * n;
+						backtracking(result, nums, expression, i + 1, '-', num * n, tmp, target);
+					}
+					expression.setLength(len);
 				}
-				expression.setLength(len);
 			}
 		}
 
