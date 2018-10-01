7efa158a19ef3e5146d061d4ddd9ad83c4a23685
kipill <kipill_90@mail.ru>
2018-10-01 19:42:39 +0200

    128. Longest Consecutive Sequence

--- src/main/java/com/leetcode/problems/unionfind/hard/LongestConsecutiveSequence.java
+++ src/main/java/com/leetcode/problems/unionfind/hard/LongestConsecutiveSequence.java
@@ -1,5 +1,6 @@
 package com.leetcode.problems.unionfind.hard;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -61,6 +62,28 @@ public class LongestConsecutiveSequence {
 				return p;
 			}
 
+		}
+	}
+
+	class NaiveSolution {
+		public int longestConsecutive(int[] nums) {
+			if (nums.length == 0) {
+				return 0;
+			}
+			Arrays.sort(nums);
+			int max = 1;
+			int count = 1;
+			for (int i = 1; i < nums.length; i++) {
+				if (nums[i] != nums[i - 1]) {
+					if (nums[i] - 1 == nums[i - 1]) {
+						count++;
+					} else {
+						count = 1;
+					}
+					max = Math.max(max, count);
+				}
+			}
+			return max;
 		}
 	}
 
--- src/test/groovy/com/leetcode/problems/unionfind/hard/LongestConsecutiveSequenceSpec.groovy
+++ src/test/groovy/com/leetcode/problems/unionfind/hard/LongestConsecutiveSequenceSpec.groovy
@@ -1,5 +1,6 @@
 package com.leetcode.problems.unionfind.hard
 
+import com.leetcode.problems.unionfind.hard.LongestConsecutiveSequence.NaiveSolution as NS
 import com.leetcode.problems.unionfind.hard.LongestConsecutiveSequence.Solution as S
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -17,6 +18,22 @@ class LongestConsecutiveSequenceSpec extends Specification {
 
 			nums                   || output
 			[100, 4, 200, 1, 3, 2] || 4
+			[1, 2, 0, 1]           || 3
+
+	}
+
+	@Unroll
+	def "longestConsecutive_naive"() {
+
+		expect:
+
+			new NS().longestConsecutive(nums as int[]) == output
+
+		where:
+
+			nums                   || output
+			[100, 4, 200, 1, 3, 2] || 4
+			[1, 2, 0, 1]           || 3
 
 	}
 
