2fd3272b8eebb1997937c670caea053cbe73ac57
kipill <kipill_90@mail.ru>
2018-10-01 16:56:12 +0200

    803. Bricks Falling When Hit scratch

--- src/main/java/com/leetcode/problems/unionfind/hard/BricksFallingWhenHit.java
+++ src/main/java/com/leetcode/problems/unionfind/hard/BricksFallingWhenHit.java
@@ -1,6 +1,7 @@
 package com.leetcode.problems.unionfind.hard;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/bricks-falling-when-hit/description/
@@ -13,8 +14,6 @@ public class BricksFallingWhenHit {
 		private final int[] dr = new int[]{-1, 0, 0, 1};
 		private final int[] dc = new int[]{0, -1, 1, 0};
 
-		private Map<Integer, List<Integer>> map = new HashMap<>();
-
 		public int[] hitBricks(int[][] grid, int[][] hits) {
 			int[] ans = new int[hits.length];
 			if (grid.length == 0) {
@@ -22,22 +21,7 @@ public class BricksFallingWhenHit {
 			}
 			int rows = grid.length;
 			int cols = grid[0].length;
-			for (int r = 0; r < rows; r++) {
-				for (int c = 0; c < cols; c++) {
-					if (grid[r][c] == 1) {
-						int curr = key(r, c, cols);
-						List<Integer> parents = new ArrayList<>();
-						for (int i = 0; i < 4; i++) {
-							int tr = r + dr[i], tc = c + dc[i];
-							if (tr >= 0 && tr < rows && tc >= 0 && tc < cols && grid[tr][tc] == 1) {
-								int neighbour = key(tr, tc, cols);
-								parents.add(neighbour);
-							}
-						}
-						map.put(curr, parents);
-					}
-				}
-			}
+
 			for (int i = 0; i < ans.length; i++) {
 				int count = -1;
 				int[] hit = hits[i];
@@ -48,7 +32,7 @@ public class BricksFallingWhenHit {
 				while (!queue.isEmpty()) {
 					int[] curr = queue.poll();
 					boolean[][] visited = new boolean[rows][cols];
-					if ((curr == hit || grid[curr[0]][curr[1]] == 1) && find(grid, visited, curr[0], curr[1], cols, -1) >= cols) {
+					if ((curr == hit || grid[curr[0]][curr[1]] == 1) && !find(grid, visited, curr[0], curr[1])) {
 						count++;
 						grid[curr[0]][curr[1]] = 0;
 						for (int j = 0; j < 4; j++) {
@@ -64,35 +48,24 @@ public class BricksFallingWhenHit {
 			return ans;
 		}
 
-		private int find(int[][] grid, boolean[][] visited, int r, int c, int cols, int from) {
+		private boolean find(int[][] grid, boolean[][] visited, int r, int c) {
 			if (visited[r][c] || grid[r][c] == 0) {
-				return Integer.MAX_VALUE;
+				return false;
+			} else if (r == 0) {
+				return true;
 			}
 			visited[r][c] = true;
-			Integer key = key(r, c, cols);
-			int min = key;
-			Iterator<Integer> iterator = map.get(key).iterator();
-			while (iterator.hasNext()) {
-				Integer p = iterator.next();
-				int[] point = point(p, cols);
-				if (grid[point[0]][point[1]] == 0) {
-					iterator.remove();
-					continue;
-				}
-				min = Math.min(min, find(grid, visited, point[0], point[1], cols, key));
-				if (min < cols) {
-					return min;
+			int rows = grid.length;
+			int cols = grid[0].length;
+			for (int j = 0; j < 4; j++) {
+				int tr = r + dr[j], tc = c + dc[j];
+				if (tr >= 0 && tr < rows && tc >= 0 && tc < cols && grid[tr][tc] == 1) {
+					if (find(grid, visited, tr, tc)) {
+						return true;
+					}
 				}
 			}
-			return min;
-		}
-
-		private int[] point(int key, int cols) {
-			return new int[]{key / cols, key % cols};
-		}
-
-		private int key(int r, int c, int cols) {
-			return r * cols + c;
+			return false;
 		}
 
 	}
