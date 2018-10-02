ddab699633250e3677819c365a3b553f7ddf71e9
kipill <kipill_90@mail.ru>
2018-10-02 14:00:02 +0200

    731. My Calendar II scratch

--- src/main/java/com/leetcode/problems/binarysearchtree/medium/MyCalendarII.java
+++ src/main/java/com/leetcode/problems/binarysearchtree/medium/MyCalendarII.java
@@ -1,7 +1,6 @@
 package com.leetcode.problems.binarysearchtree.medium;
 
-import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/my-calendar-ii/description/
@@ -9,60 +8,131 @@ import java.util.TreeSet;
 @SuppressWarnings("ConstantConditions")
 public class MyCalendarII {
 
-	/**
-	 * Your MyCalendarTwo object will be instantiated and called as such:
-	 * MyCalendarTwo obj = new MyCalendarTwo();
-	 * boolean param_1 = obj.book(start,end);
-	 */
 	class MyCalendarTwo {
 
-		private TreeSet<Time> intervals;
+		private LinkedList<Interval> intervals = new LinkedList<>();
 
 		public MyCalendarTwo() {
-			intervals = new TreeSet<>(Comparator.comparingInt(t -> t.time));
+			intervals.add(new Interval(0, Integer.MAX_VALUE, 0));
 		}
 
 		public boolean book(int start, int end) {
-			int max = 0;
-			int count = 0;
-			Time startTime = new Time(start, 1);
-			Time endTime = new Time(end, -1);
-			for (Time time : intervals.subSet(startTime, endTime)) {
-				count += time.count;
-				max = Math.max(max, Math.abs(count));
-			}
-			if (max > 1) {
+			int count = max(start, end - 1);
+			if (count > 1) {
 				return false;
 			}
-			if (intervals.contains(startTime)) {
-				Time time = intervals.ceiling(startTime);
-				time.count++;
-			} else {
-				intervals.add(startTime);
-			}
-
-			if (intervals.contains(endTime)) {
-				Time time = intervals.ceiling(endTime);
-				time.count--;
-			} else {
-				intervals.add(endTime);
-			}
+			insert(start, end);
 			return true;
 		}
 
-		private class Time {
-			int time, count;
+		private void insert(int start, int end) {
+			ListIterator<Interval> iterator = intervals.listIterator();
+			while (iterator.hasNext()) {
+				Interval curr = iterator.next();
+				if (curr.start > end) {
+					break;
+				} else if (curr.end >= start) {
+					int left = start - curr.start;
+					int right = curr.end - end;
+					if (left > 0) {
+						iterator.set(new Interval(curr.start, start - 1, curr.count));
+						iterator.add(new Interval(start, Math.min(end, curr.end), curr.count + 1));
+					} else {
+						iterator.set(new Interval(start - left, Math.min(end, curr.end), curr.count + 1));
+					}
+					if (right > 0) {
+						iterator.add(new Interval(end + 1, curr.end, curr.count));
+					}
+				}
+			}
+		}
 
-			public Time(int time, int count) {
-				this.time = time;
+		private int max(int start, int end) {
+			int max = 0;
+			for (Interval interval : intervals) {
+				if (interval.start <= start && interval.end >= end) {
+					max = Math.max(max, interval.count);
+				} else if (interval.start >= start && interval.start <= end) {
+					max = Math.max(max, interval.count);
+				} else if (interval.end >= start && interval.end <= end) {
+					max = Math.max(max, interval.count);
+				} else if (interval.start > end) {
+					break;
+				}
+			}
+			return max;
+		}
+
+		private class Interval {
+			int start, end, count;
+
+			public Interval(int start, int end, int count) {
+				this.start = start;
+				this.end = end;
 				this.count = count;
 			}
 
 			@Override
 			public String toString() {
-				return "Time{" +
-						"time=" + time +
+				return "Interval{" +
+						"start=" + start +
+						", end=" + end +
 						", count=" + count +
+						'}';
+			}
+		}
+	}
+
+	/**
+	 * Your MyCalendarTwo object will be instantiated and called as such:
+	 * MyCalendarTwo obj = new MyCalendarTwo();
+	 * boolean param_1 = obj.book(start,end);
+	 */
+	class MyCalendarTwo_ {
+
+		private TreeSet<StartTime> intervals;
+
+		public MyCalendarTwo_() {
+			intervals = new TreeSet<>(Comparator.comparingInt(t -> t.start));
+		}
+
+		public boolean book(int start, int end) {
+			int count = 0;
+			for (StartTime time : intervals.headSet(new StartTime(end))) {
+				for (Integer e : time.ends) {
+					if (time.start >= start || e <= end) {
+						count++;
+					}
+				}
+			}
+			if (count > 1) {
+				return false;
+			}
+			StartTime time = new StartTime(start);
+			if (intervals.contains(time)) {
+				time = intervals.ceiling(time);
+				time.ends.add(end);
+			} else {
+				time.ends.add(end);
+				intervals.add(time);
+			}
+
+			return true;
+		}
+
+		private class StartTime {
+			int start;
+			List<Integer> ends = new ArrayList<>();
+
+			public StartTime(int start) {
+				this.start = start;
+			}
+
+			@Override
+			public String toString() {
+				return "StartTime{" +
+						"start=" + start +
+						", ends=" + ends +
 						'}';
 			}
 		}
