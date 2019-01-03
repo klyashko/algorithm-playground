16a6e2aabfde2dd31caa175b1278e185bab38695
kipill <kipill_90@mail.ru>
2019-01-03 22:18:35 +0100

    773. Sliding Puzzle

--- src/main/java/com/leetcode/problems/breadthfirstsearch/hard/SlidingPuzzle.java
+++ src/main/java/com/leetcode/problems/breadthfirstsearch/hard/SlidingPuzzle.java
@@ -1,6 +1,9 @@
 package com.leetcode.problems.breadthfirstsearch.hard;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/sliding-puzzle/
@@ -10,75 +13,55 @@ public class SlidingPuzzle {
 
 	class Solution {
 
-		private final int[] dr = new int[]{1, -1, 0, 0};
-		private final int[] dc = new int[]{0, 0, 1, -1};
+		private final int[][] moves = new int[][]{
+				{1, 3},
+				{0, 2, 4},
+				{1, 5},
+				{0, 4},
+				{1, 3, 5},
+				{2, 4}
+		};
+
+		private final String end = "123450";
 
 		public int slidingPuzzle(int[][] board) {
-			Queue<Node> queue = new ArrayDeque<>();
+			StringBuilder builder = new StringBuilder();
+			for (int[] r : board) {
+				for (int i : r) {
+					builder.append(i);
+				}
+			}
+			Queue<String> queue = new ArrayDeque<>();
 			Set<String> seen = new HashSet<>();
-			Node start = new Node(board);
+			String start = builder.toString();
 			queue.offer(start);
-			seen.add(start.key());
+			seen.add(start);
 
-			int move = 0;
-
-			while (!queue.isEmpty()) {
-				int size = queue.size();
-				for (; size > 0; size--) {
-					Node n = queue.poll();
-					if (n.score == 1 && n.value[1][2] == 0) {
+			for (int move = 0; !queue.isEmpty(); move++) {
+				for (int size = queue.size(); size > 0; size--) {
+					String curr = queue.poll();
+					if (curr.equals(end)) {
 						return move;
 					}
-					for (int r = 0; r < n.value.length; r++) {
-						int[] row = n.value[r];
-						for (int c = 0; c < row.length; c++) {
-							if (row[c] == 0) {
-								for (int i = 0; i < 4; i++) {
-									int tr = r + dr[i], tc = c + dc[i];
-									if (tr < 2 && tr >= 0 && tc < 3 && tc >= 0) {
-										n.value[r][c] = n.value[tr][tc];
-										n.value[tr][tc] = 0;
-										Node next = new Node(n.value);
-										if (seen.add(next.key())) {
-											queue.offer(next);
-										}
-										n.value[tr][tc] = n.value[r][c];
-										n.value[r][c] = 0;
-									}
-								}
-							}
+					int idx = curr.indexOf('0');
+					for (int m : this.moves[idx]) {
+						String next = swap(curr, idx, m);
+						if (seen.add(next)) {
+							queue.offer(next);
 						}
 					}
 				}
-				move += 1;
 			}
 
 			return -1;
 		}
 
-		private class Node {
-
-			int[][] value;
-			int score;
-
-			public Node(int[][] value) {
-				int[][] tmp = new int[value.length][];
-				for (int i = 0; i < value.length; i++) {
-					tmp[i] = Arrays.copyOf(value[i], value[i].length);
-				}
-				this.value = tmp;
-				for (int i = 0; i < 6; i++) {
-					int r = i / 3;
-					int c = i % 3;
-					if (tmp[r][c] != i + 1) {
-						score += 1;
-					}
-				}
-			}
-
-			private String key() {
-				return Arrays.toString(value[0]) + Arrays.toString(value[1]);
-			}
+		private String swap(String s, int i, int j) {
+			StringBuilder builder = new StringBuilder(s);
+			char ch = builder.charAt(i);
+			builder.setCharAt(i, builder.charAt(j));
+			builder.setCharAt(j, ch);
+			return builder.toString();
 		}
 	}
 
