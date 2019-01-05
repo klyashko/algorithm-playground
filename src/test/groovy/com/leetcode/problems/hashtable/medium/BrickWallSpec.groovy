000a958522ebc6d8e5ac4c11023407bacef3cd46
kipill <kipill_90@mail.ru>
2019-01-05 10:49:24 +0100

    554. Brick Wall

---
+++ src/test/groovy/com/leetcode/problems/hashtable/medium/BrickWallSpec.groovy
@@ -0,0 +1,31 @@
+package com.leetcode.problems.hashtable.medium
+
+import com.leetcode.problems.hashtable.medium.BrickWall.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class BrickWallSpec extends Specification {
+
+	@Unroll
+	def "leastBricks"() {
+
+		expect:
+
+			new S().leastBricks(wall) == out
+
+		where:
+
+			wall                                    || out
+			[[1, 2, 2, 1]]                          || 0
+			[[1, 2, 2, 1],
+			 [3, 1, 2],
+			 [1, 3, 2],
+			 [2, 4],
+			 [3, 1, 2],
+			 [1, 3, 1, 1]]                          || 2
+			[[1], [1], [1]]                         || 3
+			[[100000000], [100000000], [100000000]] || 3
+
+	}
+
+}
