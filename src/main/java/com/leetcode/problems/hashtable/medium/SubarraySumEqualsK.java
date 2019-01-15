69f265b24d031d07e48a9c515ed247fa301beb19
kipill <kipill_90@mail.ru>
2019-01-15 19:53:13 +0100

    560. Subarray Sum Equals K

--- src/main/java/com/leetcode/problems/hashtable/medium/SubarraySumEqualsK.java
+++ src/main/java/com/leetcode/problems/hashtable/medium/SubarraySumEqualsK.java
@@ -1,11 +1,30 @@
 package com.leetcode.problems.hashtable.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/subarray-sum-equals-k/
  */
 public class SubarraySumEqualsK {
 
 	class Solution {
+		public int subarraySum(int[] nums, int k) {
+			Map<Integer, Integer> sums = new HashMap<>();
+			int count = 0, sum = 0;
+
+			sums.put(0, 1);
+			for (int i : nums) {
+				sum += i;
+				count += sums.getOrDefault(sum - k, 0);
+				sums.put(sum, sums.getOrDefault(sum, 0) + 1);
+			}
+
+			return count;
+		}
+	}
+
+	class CamSumSolution {
 		public int subarraySum(int[] nums, int k) {
 			int[] col = new int[nums.length];
 
--- src/test/groovy/com/leetcode/problems/hashtable/medium/SubarraySumEqualsKSpec.groovy
+++ src/test/groovy/com/leetcode/problems/hashtable/medium/SubarraySumEqualsKSpec.groovy
@@ -1,5 +1,6 @@
 package com.leetcode.problems.hashtable.medium
 
+import com.leetcode.problems.hashtable.medium.SubarraySumEqualsK.CamSumSolution as CSS
 import com.leetcode.problems.hashtable.medium.SubarraySumEqualsK.Solution as S
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -12,6 +13,20 @@ class SubarraySumEqualsKSpec extends Specification {
 		expect:
 
 			new S().subarraySum(nums as int[], k) == out
+
+		where:
+
+			nums      | k || out
+			[1, 1, 1] | 2 || 2
+
+	}
+
+	@Unroll
+	def "subarraySum_CamSum"() {
+
+		expect:
+
+			new CSS().subarraySum(nums as int[], k) == out
 
 		where:
 
