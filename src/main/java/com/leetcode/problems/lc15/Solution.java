19043419869a6c065c5fa562da253dd39631215a
kipill <kipill_90@mail.ru>
2019-02-19 21:05:45 +0100

    15. 3Sum

--- src/main/java/com/leetcode/problems/lc15/Solution.java
+++ src/main/java/com/leetcode/problems/lc15/Solution.java
@@ -1,6 +1,8 @@
 package com.leetcode.problems.lc15;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * https://leetcode.com/problems/3sum/
@@ -13,8 +15,8 @@ public class Solution {
 		int prev = Integer.MAX_VALUE;
 		for (int i = 0; i < nums.length; i++) {
 			if (prev != nums[i]) {
-				for (Pair two : twoSum(nums, i + 1, 0 - nums[i])) {
-					values.add(list(nums[i], two.left, two.right));
+				for (int[] two : twoSum(nums, i + 1, 0 - nums[i])) {
+					values.add(list(nums[i], two[0], two[1]));
 				}
 				prev = nums[i];
 			}
@@ -22,9 +24,9 @@ public class Solution {
 		return values;
 	}
 
-	private Set<Pair> twoSum(int[] nums, int from, int sum) {
+	private List<int[]> twoSum(int[] nums, int from, int sum) {
 		int l = from, r = nums.length - 1;
-		Set<Pair> result = new HashSet<>();
+		List<int[]> result = new ArrayList<>();
 		while (l < r) {
 			int curr = nums[l] + nums[r];
 			if (curr < sum) {
@@ -32,7 +34,7 @@ public class Solution {
 			} else if (curr > sum) {
 				r--;
 			} else {
-				result.add(new Pair(nums[l], nums[r]));
+				result.add(new int[]{nums[l], nums[r]});
 				int left = nums[l];
 				while (l < nums.length && left == nums[l]) {
 					l++;
@@ -44,29 +46,6 @@ public class Solution {
 
 	private List<Integer> list(Integer i1, Integer i2, Integer i3) {
 		return new ArrayList<>(Arrays.asList(i1, i2, i3));
-	}
-
-	private static class Pair {
-		int left, right;
-
-		Pair(int left, int right) {
-			this.left = left;
-			this.right = right;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-			Pair pair = (Pair) o;
-			return left == pair.left &&
-					right == pair.right;
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(left, right);
-		}
 	}
 
 }
