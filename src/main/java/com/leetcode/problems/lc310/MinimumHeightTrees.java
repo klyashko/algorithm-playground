f0c6fa6f992f2f7be03fa9960a21ba400e9a25db
kipill <kipill_90@mail.ru>
2019-03-04 21:44:16 +0100

    310. Minimum Height Trees

--- src/main/java/com/leetcode/problems/lc310/MinimumHeightTrees.java
+++ src/main/java/com/leetcode/problems/lc310/MinimumHeightTrees.java
@@ -13,19 +13,16 @@ public class MinimumHeightTrees {
 			if (n == 1) {
 				return Collections.singletonList(0);
 			}
-			int[] degrees = new int[n];
 			Map<Integer, Set<Integer>> graph = new HashMap<>();
 			for (int[] edge : edges) {
 				graph.computeIfAbsent(edge[0], ignored -> new HashSet<>()).add(edge[1]);
 				graph.computeIfAbsent(edge[1], ignored -> new HashSet<>()).add(edge[0]);
-				degrees[edge[0]]++;
-				degrees[edge[1]]++;
 			}
 
 			Queue<Integer> leafs = new ArrayDeque<>();
-			for (int i = 0; i < degrees.length; i++) {
-				if (degrees[i] == 1) {
-					leafs.offer(i);
+			for (Map.Entry<Integer, Set<Integer>> entry : graph.entrySet()) {
+				if (entry.getValue().size() == 1) {
+					leafs.offer(entry.getKey());
 				}
 			}
 
@@ -33,13 +30,12 @@ public class MinimumHeightTrees {
 				int size = leafs.size();
 				for (int i = 0; i < size; i++) {
 					int curr = leafs.poll();
-					degrees[curr] = 0;
 					for (Integer parent : graph.remove(curr)) {
-						degrees[parent]--;
-						if (degrees[parent] == 1) {
+						Set<Integer> children = graph.get(parent);
+						children.remove(curr);
+						if (children.size() == 1) {
 							leafs.offer(parent);
 						}
-						graph.get(parent).remove(curr);
 					}
 				}
 			}
