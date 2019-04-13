a0bfb06db9b25ee7df5a979cf76077ffec14de94
kipill <kipill_90@mail.ru>
2019-04-13 16:19:33 +0200

    Alien Rhyme

--- src/main/java/com/algorithm/playground/google/codejam/_2019/a/task3/Solution.java
+++ src/main/java/com/algorithm/playground/google/codejam/_2019/a/task3/Solution.java
@@ -1,9 +1,6 @@
 package com.algorithm.playground.google.codejam._2019.a.task3;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 /**
  * https://codingcompetitions.withgoogle.com/codejam/round/0000000000051635/0000000000104e05
@@ -29,34 +26,39 @@ public class Solution {
 
 	static int solve(String[] words) {
 		Trie trie = new Trie();
-		int max = 0;
 		for (String word : words) {
 			trie.insert(word);
-			max = Math.max(max, word.length());
 		}
-		Set<String> used = new HashSet<>();
-		Set<String> set = new HashSet<>();
-		for (int i = 0; i < max; i++) {
-			for (String w : words) {
-				if (w.length() > i && !used.contains(w)) {
-					String suffix = w.substring(i);
-					if (!set.contains(suffix)) {
-						Set<String> options = trie.search(suffix);
-						for (String op : options) {
-							if (!op.equals(w) && !used.contains(op)) {
-								used.add(w);
-								used.add(op);
 
-								set.add(suffix);
-								break;
-							}
-						}
+		Queue<Pair> pairs = new PriorityQueue<>(Comparator.comparingInt((Pair p) -> p.suffix.length()).reversed());
+
+		for (String word : words) {
+			pairs.addAll(trie.search(word));
+		}
+
+		Set<String> used = new HashSet<>();
+		Set<String> suffixes = new HashSet<>();
+		while (!pairs.isEmpty()) {
+			Pair curr = pairs.poll();
+			if (!used.contains(curr.word1) && !used.contains(curr.word2)) {
+				if (suffixes.contains(curr.suffix)) {
+					String suffix = curr.suffix.substring(1);
+					while (!suffix.isEmpty() && suffixes.contains(suffix)) {
+						suffix = suffix.substring(1);
 					}
+					if (!suffix.isEmpty()) {
+						pairs.offer(new Pair(curr.word1, curr.word2, suffix));
+					}
+				} else {
+//					System.out.println(curr);
+					used.add(curr.word1);
+					used.add(curr.word2);
+
+					suffixes.add(curr.suffix);
 				}
 			}
 		}
-//		System.out.println(used);
-//		System.out.println(set);
+
 		return used.size();
 	}
 
@@ -80,34 +82,77 @@ public class Solution {
 				if (curr.children[idx] == null) {
 					curr.children[idx] = new TrieNode();
 				}
-				curr.count++;
 				curr = curr.children[idx];
-				curr.words.add(word);
 			}
-			curr.isWord = true;
+			curr.word = word;
+		}
+
+		public Set<Pair> search(String word) {
+			Set<Pair> pairs = new HashSet<>();
+			StringBuilder suffix = new StringBuilder();
+			int idx = word.length() - 1;
+			char ch = word.charAt(idx);
+			TrieNode node = root.children[ch - 'A'];
+
+			suffix.append(ch);
+
+			search(node, word, suffix, pairs, idx - 1, false);
+			return pairs;
 		}
 
 		/**
 		 * Returns if the word is in the trie.
 		 */
-		public Set<String> search(String word) {
-			TrieNode curr = root;
-			for (int i = word.length() - 1; i >= 0; i--) {
-				char ch = word.charAt(i);
-				int idx = ch - 'A';
-				if (curr.children[idx] == null) {
-					return Collections.emptySet();
+		private void search(TrieNode node, String word, StringBuilder suffix, Set<Pair> pairs, int idx, boolean closed) {
+			if (node != null) {
+				if (node.word != null && !word.equals(node.word)) {
+					pairs.add(new Pair(word, node.word, suffix.toString()));
 				}
-				curr = curr.children[idx];
+				if (idx >= 0) {
+					char ch = word.charAt(idx);
+					for (int i = 0; i < node.children.length; i++) {
+						TrieNode next = node.children[i];
+						char val = (char) (i + 'A');
+						if (!closed && ch == val) {
+							if (idx != 0) {
+								suffix.insert(0, val);
+								search(next, word, suffix, pairs, idx - 1, false);
+								suffix.deleteCharAt(0);
+							}
+						} else {
+							search(next, word, suffix, pairs, idx, true);
+						}
+					}
+				}
 			}
-			return curr == null ? Collections.emptySet() : curr.words;
 		}
 
 		private class TrieNode {
 			TrieNode[] children = new TrieNode[26];
-			Set<String> words = new HashSet<>();
-			boolean isWord;
-			int count = 0;
+			String word;
+		}
+
+	}
+
+	private static class Pair {
+
+		private String word1;
+		private String word2;
+		private String suffix;
+
+		private Pair(String word1, String word2, String suffix) {
+			this.word1 = word1;
+			this.word2 = word2;
+			this.suffix = suffix;
+		}
+
+		@Override
+		public String toString() {
+			return "Pair{" +
+					"word1='" + word1 + '\'' +
+					", word2='" + word2 + '\'' +
+					", suffix='" + suffix + '\'' +
+					'}';
 		}
 	}
 
--- src/test/groovy/com/algorithm/playground/google/codejam/_2019/a/task3/AlienRhymeSpec.groovy
+++ src/test/groovy/com/algorithm/playground/google/codejam/_2019/a/task3/AlienRhymeSpec.groovy
@@ -14,23 +14,59 @@ class AlienRhymeSpec extends Specification {
 
 		where:
 
-			words      || out
+			words           || out
 			['TARPOL',
-			 'PROL']   || 2
+			 'PROL']        || 2
 			['TARPOR',
 			 'PROL',
-			 'TARPRO'] || 0
+			 'TARPRO']      || 0
 			['CODEJAM',
 			 'JAM',
 			 'HAM',
 			 'NALAM',
 			 'HUM',
-			 'NOLOM']  || 6
+			 'NOLOM']       || 6
 			['PI',
 			 'HI',
 			 'WI',
-			 'FI']     || 2
+			 'FI']          || 2
+			['EDBEFEACA',
+			 'BADAFACEEAED',
+			 'C',
+			 'EADBACDCFFDBBCFAA',
+			 'CCBBDECAFDF'] || 2
 
 	}
+
+//	def "test"() {
+//
+//		given:
+//
+////			for (int i = 0; i < 1000000; i++) {
+//			for (int i = 0; i < 100; i++) {
+//				def words = []
+//				1000.times {
+//					words << randomUUID().toString()
+//							.replaceAll("[0-9]", "")
+//							.replaceAll("-", "")
+//							.toUpperCase()
+//				}
+//
+//				def right = Solution_.solve(words as String[])
+//				def mine = Solution.solve(words as String[])
+//
+//				if (right != mine) {
+//					println(right)
+//					println(mine)
+//					words.each { println(it) }
+//					break
+//				}
+//
+//				if (i % 100 == 0) {
+//					println "Iteration ${i}"
+//				}
+//			}
+//
+//	}
 
 }
