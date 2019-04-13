24062064cbb57d3068cafa7b5ed4e7e0ef844f48
kipill <kipill_90@mail.ru>
2019-04-13 13:41:44 +0200

    Golf Gophers

---
+++ src/main/java/com/algorithm/playground/google/codejam/_2019/a/task2/Solution.java
@@ -0,0 +1,26 @@
+package com.algorithm.playground.google.codejam._2019.a.task2;
+
+import java.util.Scanner;
+
+/**
+ * https://codingcompetitions.withgoogle.com/codejam/round/0000000000051635/0000000000104f1a
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		try (Scanner console = new Scanner(System.in)) {
+			int tests = console.nextInt();
+			for (int test = 1; test <= tests; test++) {
+
+				//				System.out.println(String.format("Case #%s: %s", test, ans));
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+	}
+
+	static void solve() {
+
+	}
+
+}
---
+++ src/test/groovy/com/algorithm/playground/google/codejam/_2019/a/task2/GolfGophersSpec.groovy
@@ -0,0 +1,20 @@
+package com.algorithm.playground.google.codejam._2019.a.task2
+
+import spock.lang.Specification
+
+class GolfGophersSpec extends Specification {
+
+//	@Unroll
+//	def "solve"(){
+//
+//		expect:
+//
+//			Solution.solve(input) == out
+//
+//		where:
+//
+//			input || out
+//
+//	}
+
+}
