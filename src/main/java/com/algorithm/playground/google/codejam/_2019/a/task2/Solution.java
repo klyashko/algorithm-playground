7837141b0a5d463737e3e0c4a2b83a26f14556a1
kipill <kipill_90@mail.ru>
2019-04-16 20:00:19 +0200

    Golf Gophers

--- src/main/java/com/algorithm/playground/google/codejam/_2019/a/task2/Solution.java
+++ src/main/java/com/algorithm/playground/google/codejam/_2019/a/task2/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
 			//Number of gopher
 			int m = console.nextInt();
 			for (int test = 1; test <= tests; test++) {
-				int ans = n == 7 ? solveBig(m) : solveSmall();
+				int ans = solve(n, m);
 				System.out.println(ans);
 				if (console.nextInt() == -1) {
 					throw new RuntimeException("WA");
@@ -31,11 +31,12 @@ public class Solution {
 		}
 	}
 
-	private static int solveBig(int m) {
-		int[] mods = new int[7];
-		for (int i = 0; i < 7; i++) {
+	private static int solve(int n, int m) {
+		int[] mods = new int[n];
+		for (int i = 0; i < n; i++) {
+			int curr = BLADES[i % BLADES.length];
 			for (int j = 0; j < N; j++) {
-				System.out.print(BLADES[i]);
+				System.out.print(curr);
 				System.out.print(' ');
 			}
 			System.out.println();
@@ -43,12 +44,12 @@ public class Solution {
 			for (int j = 0; j < N; j++) {
 				sum += console.nextInt();
 			}
-			mods[i] = sum % BLADES[i];
+			mods[i] = sum % curr;
 		}
 		int ans = 0;
 		for (int i = 1; i <= m && ans == 0; i++) {
-			for (int j = 0; j < BLADES.length; j++) {
-				if (i % BLADES[j] == mods[j]) {
+			for (int j = 0; j < n; j++) {
+				if (i % BLADES[j % BLADES.length] == mods[j]) {
 					ans = i;
 				} else {
 					ans = 0;
@@ -57,25 +58,6 @@ public class Solution {
 			}
 		}
 		return ans;
-	}
-
-	private static int solveSmall() {
-		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			builder.append("18 ");
-		}
-		String request = builder.toString().trim();
-
-		int max = 0;
-		for (int i = 0; i < 365; i++) {
-			System.out.println(request);
-			int curr = 0;
-			for (int j = 0; j < N; j++) {
-				curr += console.nextInt();
-			}
-			max = Math.max(max, curr);
-		}
-		return max;
 	}
 
 }
