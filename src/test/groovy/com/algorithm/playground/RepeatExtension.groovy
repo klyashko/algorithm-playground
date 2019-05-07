02d8607e26ef35bfeab2afb876988e2d1852b450
kipill <kipill_90@mail.ru>
2019-05-07 23:54:11 +0200

    refactoring

---
+++ src/test/groovy/com/algorithm/playground/RepeatExtension.groovy
@@ -0,0 +1,13 @@
+package com.algorithm.playground
+
+import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension
+import org.spockframework.runtime.model.FeatureInfo
+
+class RepeatExtension extends AbstractAnnotationDrivenExtension<Repeat> {
+
+	@Override
+	void visitFeatureAnnotation(Repeat annotation, FeatureInfo feature) {
+		feature.reportIterations = true
+		feature.addInterceptor(new RepeatInterceptor(annotation.value()))
+	}
+}
