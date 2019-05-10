7d68252f3661f8a63e82296882c41b6cc1683ca3
kipill <kipill_90@mail.ru>
2019-05-10 20:19:30 +0200

    955. Delete Columns to Make Sorted II

--- src/main/java/com/algorithm/playground/leetcode/problems/lc900/lc955/DeleteColumnsToMakeSortedII.java
+++ src/main/java/com/algorithm/playground/leetcode/problems/lc900/lc955/DeleteColumnsToMakeSortedII.java
@@ -13,15 +13,19 @@ public class DeleteColumnsToMakeSortedII {
 			char[][] chars = new char[A.length][len];
 			for (int i = 0; i < len; i++) {
 				boolean delete = false;
+				int same = 0;
 				for (int j = 1; j < A.length && !delete; j++) {
 					String s1 = A[j - 1];
 					String s2 = A[j];
 					if (Arrays.equals(chars[j - 1], chars[j])) {
 						delete = s2.charAt(i) < s1.charAt(i);
+						same += s2.charAt(i) == s1.charAt(i) ? 1 : 0;
 					}
 				}
 				if (delete) {
 					count++;
+				} else if (same == 0) {
+					break;
 				} else {
 					for (int j = 0; j < A.length; j++) {
 						chars[j][i] = A[j].charAt(i);
