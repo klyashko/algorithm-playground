e24b1a56d8f8d29811dfd502e64215181e7c0814
kipill <kipill_90@mail.ru>
2019-05-10 21:14:32 +0200

    45. Jump Game II

--- src/main/java/com/algorithm/playground/leetcode/problems/lc0/lc40/lc45/JumpGameII.java
+++ src/main/java/com/algorithm/playground/leetcode/problems/lc0/lc40/lc45/JumpGameII.java
@@ -5,26 +5,29 @@ package com.algorithm.playground.leetcode.problems.lc0.lc40.lc45;
  */
 public class JumpGameII {
 
-    class Solution {
-        public int jump(int[] nums) {
-            int jumps = 0;
-            for (int i = 0; i < nums.length - 1; ) {
-                jumps++;
-                if (i + nums[i] + 1 >= nums.length) {
-                    return jumps;
-                }
-                int next = i + 1;
-                int nextGain = nums[next] + i;
-                for (int j = next; j < nums[i] + i + 1; j++) {
-                    if (nums[j] + j > nextGain) {
-                        next = j;
-                        nextGain = nums[j] + j;
-                    }
-                }
-                i = next;
-            }
-            return jumps;
-        }
-    }
+	class Solution {
+		public int jump(int[] nums) {
+			int jumps = 0;
+			for (int i = 0; i < nums.length - 1; ) {
+				jumps++;
+				if (i + nums[i] + 1 >= nums.length) {
+					return jumps;
+				}
+				int next = i + 1;
+				int nextGain = nums[next] + i;
+				for (int j = next; j < nums[i] + i + 1; j++) {
+					if (nums[j] + j > nextGain) {
+						next = j;
+						nextGain = nums[j] + j;
+					}
+					if (nextGain >= nums.length) {
+						return jumps + 1;
+					}
+				}
+				i = next;
+			}
+			return jumps;
+		}
+	}
 
 }
