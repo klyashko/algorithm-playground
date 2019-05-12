0d300b664bfa0b4fd6735e6d4014e3d734e0b00c
kipill <kipill_90@mail.ru>
2019-05-12 19:58:09 +0200

    refactoring

--- src/main/java/com/algorithm/playground/google/kickstart/_2019/b/task3/Solution_.java
+++ src/main/java/com/algorithm/playground/google/kickstart/_2019/b/task3/Solution_.java
@@ -25,7 +25,7 @@ public class Solution_ {
 			);
 		}
 
-		void iset(int level, int index, int value) {
+		void insert(int level, int index, int value) {
 			sum[level][index] = value;
 			max_psum[level][index] = value;
 			while (level > 0) {
@@ -46,18 +46,19 @@ public class Solution_ {
 		public void solve(int testNumber, Scanner in) {
 			int N = in.nextInt(), s = in.nextInt();
 			int[] M = readIntArray(N, in);
-			int depth = 18;
+//			int depth = 18;
+			int depth = 3;
 			sum = new int[depth + 1][];
-			for (int l = 0; l <= depth; ++l) {
+			for (int l = 0; l <= depth; l++) {
 				sum[l] = new int[1 << l];
 			}
 			max_psum = new int[depth + 1][];
-			for (int l = 0; l <= depth; ++l) {
+			for (int l = 0; l <= depth; l++) {
 				max_psum[l] = new int[1 << l];
 			}
 
 			Map<Integer, List<Integer>> occurrences = new HashMap<>();
-			for (int n = 0; n < N; ++n) {
+			for (int n = 0; n < N; n++) {
 				if (!occurrences.containsKey(M[n])) {
 					occurrences.put(M[n], new ArrayList<>());
 				}
@@ -70,30 +71,40 @@ public class Solution_ {
 			}
 
 			for (int n = 0; n < (1 << depth); ++n) {
-				iset(depth, n, 0);
+				insert(depth, n, 0);
 			}
 			for (int scene : occurrences.keySet()) {
 				for (int j = 1; j <= s && j - 1 < occurrences.get(scene).size(); j++) {
-					iset(depth, occurrences.get(scene).get(j - 1), +1);
+					insert(depth, occurrences.get(scene).get(j - 1), 1);
 				}
 				if (occurrences.get(scene).size() >= s + 1) {
-					iset(depth, occurrences.get(scene).get(s), -s);
+					insert(depth, occurrences.get(scene).get(s), -s);
 				}
 			}
 
+			for (int[] r : sum) {
+				System.out.println(Arrays.toString(r));
+			}
+			System.out.println();
+			for (int[] r : max_psum) {
+				System.out.println(Arrays.toString(r));
+			}
+
 			int answer = 0;
-			for (int start = 0; start < N; ++start) {
-				if (max_psum[0][0] > answer) answer = max_psum[0][0];
+			for (int start = 0; start < N; start++) {
+				if (max_psum[0][0] > answer) {
+					answer = max_psum[0][0];
+				}
 
 				int scene = M[start];
 				int off = offsets.get(scene);
-				iset(depth, start, 0);
+				insert(depth, start, 0);
 
 				if (occurrences.get(scene).size() >= s + 1 + off) {
-					iset(depth, occurrences.get(scene).get(s + off), +1);
+					insert(depth, occurrences.get(scene).get(s + off), 1);
 				}
 				if (occurrences.get(scene).size() >= s + 2 + off) {
-					iset(depth, occurrences.get(scene).get(s + 1 + off), -s);
+					insert(depth, occurrences.get(scene).get(s + 1 + off), -s);
 				}
 				offsets.put(scene, off + 1);
 			}
