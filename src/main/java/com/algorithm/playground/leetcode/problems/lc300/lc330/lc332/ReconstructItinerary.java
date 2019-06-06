8b348526c36e7879ce66d1404e7aa790545ceed8
kipill <kipill_90@mail.ru>
2019-06-06 19:48:18 +0200

    332. Reconstruct Itinerary

--- src/main/java/com/algorithm/playground/leetcode/problems/lc300/lc330/lc332/ReconstructItinerary.java
+++ src/main/java/com/algorithm/playground/leetcode/problems/lc300/lc330/lc332/ReconstructItinerary.java
@@ -9,62 +9,23 @@ public class ReconstructItinerary {
 
 	class Solution {
 		public List<String> findItinerary(List<List<String>> tickets) {
-			String start = "JFK";
-			Map<String, List<Edge>> edges = new HashMap<>();
+			Map<String, Queue<String>> edges = new HashMap<>();
 			for (List<String> e : tickets) {
 				if (!edges.containsKey(e.get(0))) {
-					edges.put(e.get(0), new ArrayList<>());
+					edges.put(e.get(0), new PriorityQueue<>());
 				}
-				edges.get(e.get(0)).add(new Edge(e.get(0), e.get(1)));
+				edges.get(e.get(0)).offer(e.get(1));
 			}
-			for (List<Edge> list : edges.values()) {
-				list.sort(Edge::compareTo);
+			return dfs(edges, "JFK", new ArrayList<>());
+		}
+
+		private List<String> dfs(Map<String, Queue<String>> edges, String curr, List<String> itinerary) {
+			Queue<String> queue = edges.get(curr);
+			while (queue != null && !queue.isEmpty()) {
+				dfs(edges, queue.poll(), itinerary);
 			}
-			List<String> itinerary = new ArrayList<>();
-			dfs(edges, start, new HashSet<>(), itinerary, tickets.size() + 1);
+			itinerary.add(0, curr);
 			return itinerary;
-		}
-
-		private boolean dfs(Map<String, List<Edge>> edges, String curr, Set<Edge> seen, List<String> itinerary, int n) {
-			itinerary.add(curr);
-			if (itinerary.size() == n) {
-				return true;
-			} else if (edges.containsKey(curr)) {
-				for (Edge e : edges.getOrDefault(curr, Collections.emptyList())) {
-					if (seen.add(e)) {
-						if (dfs(edges, e.to, seen, itinerary, n)) {
-							return true;
-						} else {
-							seen.remove(e);
-						}
-					}
-				}
-			}
-			itinerary.remove(itinerary.size() - 1);
-			return false;
-		}
-
-		private class Edge implements Comparable<Edge> {
-
-			private final String from, to;
-
-			private Edge(String from, String to) {
-				this.from = from;
-				this.to = to;
-			}
-
-			@Override
-			public int compareTo(Edge o) {
-				return to.compareTo(o.to);
-			}
-
-			@Override
-			public String toString() {
-				return "Edge{" +
-						"from='" + from + '\'' +
-						", to='" + to + '\'' +
-						'}';
-			}
 		}
 
 	}
