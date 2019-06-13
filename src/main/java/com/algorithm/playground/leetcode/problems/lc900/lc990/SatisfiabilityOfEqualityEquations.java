3353e364e69048718ef27501856e9185927ee118
kipill <kipill_90@mail.ru>
2019-06-14 01:00:46 +0200

    990. Satisfiability of Equality Equations

--- src/main/java/com/algorithm/playground/leetcode/problems/lc900/lc990/SatisfiabilityOfEqualityEquations.java
+++ src/main/java/com/algorithm/playground/leetcode/problems/lc900/lc990/SatisfiabilityOfEqualityEquations.java
@@ -1,6 +1,6 @@
 package com.algorithm.playground.leetcode.problems.lc900.lc990;
 
-import java.util.*;
+import java.util.Arrays;
 
 /**
  * https://leetcode.com/problems/satisfiability-of-equality-equations/
@@ -13,9 +13,7 @@ public class SatisfiabilityOfEqualityEquations {
 			int idx = 0, len = equations.length;
 			for (String eq : equations) {
 				if (eq.charAt(1) == '=') {
-					int i1 = eq.charAt(0) - 'a';
-					int i2 = eq.charAt(3) - 'a';
-					dsu.union(i1, i2);
+					dsu.union(eq.charAt(0), eq.charAt(3));
 					len--;
 				} else {
 					equations[idx++] = eq;
@@ -23,9 +21,7 @@ public class SatisfiabilityOfEqualityEquations {
 			}
 			for (String eq : Arrays.copyOf(equations, len)) {
 				if (eq.charAt(1) == '!') {
-					int i1 = eq.charAt(0) - 'a';
-					int i2 = eq.charAt(3) - 'a';
-					if (dsu.find(i1) == dsu.find(i2)) {
+					if (dsu.find(eq.charAt(0)) == dsu.find(eq.charAt(3))) {
 						return false;
 					}
 				}
@@ -35,113 +31,23 @@ public class SatisfiabilityOfEqualityEquations {
 
 		class DSU {
 
-			private int[] groups = new int[26];
+			private int[] groups = new int[27];
 
-			private DSU() {
-				for (int i = 0; i < groups.length; i++) {
-					groups[i] = i;
-				}
+			private int find(char key) {
+				return find(key - 'a' + 1);
 			}
 
 			private int find(int key) {
-				if (groups[key] == key) {
+				if (groups[key] == key || groups[key] == 0) {
 					return key;
 				}
 				return groups[key] = find(groups[key]);
 			}
 
-			private void union(int key1, int key2) {
+			private void union(char key1, char key2) {
 				groups[find(key1)] = find(key2);
 			}
 
-		}
-	}
-
-	class Solution_ {
-		public boolean equationsPossible(String[] equations) {
-			Map<Node, Set<Node>> edges = new HashMap<>();
-			for (String eq : equations) {
-				if (parse(eq, edges)) {
-					return false;
-				}
-			}
-			for (char ch = 'a'; ch <= 'z'; ch++) {
-				if (dfs(edges, new Node(ch, false), ch, new HashSet<>())) {
-					return false;
-				}
-			}
-
-			return true;
-		}
-
-		private boolean dfs(Map<Node, Set<Node>> edges, Node curr, char ch, Set<Node> visited) {
-			if (visited.add(curr)) {
-				for (Node next : edges.getOrDefault(curr, Collections.emptySet())) {
-					if (next.ch == ch) {
-						if (next.negate) {
-							return true;
-						}
-					} else if (!curr.negate) {
-						if (dfs(edges, next, ch, visited)) {
-							return true;
-						}
-					}
-				}
-			}
-			return false;
-		}
-
-		private boolean parse(String eq, Map<Node, Set<Node>> edges) {
-			char ch1 = eq.charAt(0);
-			char ch2 = eq.charAt(3);
-			boolean negate = '!' == eq.charAt(1);
-			if (ch1 == ch2 && negate) {
-				return true;
-			} else if (ch1 != ch2) {
-				put(edges, new Node(ch2, false), new Node(ch1, negate));
-				put(edges, new Node(ch1, false), new Node(ch2, negate));
-			}
-			return false;
-		}
-
-		private void put(Map<Node, Set<Node>> edges, Node key, Node node) {
-			if (!edges.containsKey(key)) {
-				edges.put(key, new HashSet<>());
-			}
-			edges.get(key).add(node);
-		}
-
-		private class Node {
-
-			private final Character ch;
-			private final boolean negate;
-
-			private Node(char ch, boolean negate) {
-				this.ch = ch;
-				this.negate = negate;
-			}
-
-			@Override
-			public boolean equals(Object o) {
-				if (this == o) return true;
-				if (o == null || getClass() != o.getClass()) return false;
-				Node node = (Node) o;
-				return ch == node.ch &&
-						negate == node.negate;
-			}
-
-			@Override
-			public int hashCode() {
-				return Objects.hash(ch, negate);
-			}
-
-			@Override
-			public String toString() {
-				return "Node{" +
-						"ch=" + ch +
-						", negate=" + negate +
-						'}';
-			}
 		}
 	}
 
