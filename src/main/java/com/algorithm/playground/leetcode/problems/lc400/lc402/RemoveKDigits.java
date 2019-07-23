048e91217fd8f5cd67f6367ca34049e0f49b3eac
kipill <kipill_90@mail.ru>
2019-07-23 19:10:31 +0200

    402. Remove K Digits

---
+++ src/main/java/com/algorithm/playground/leetcode/problems/lc400/lc402/RemoveKDigits.java
@@ -0,0 +1,50 @@
+package com.algorithm.playground.leetcode.problems.lc400.lc402;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * https://leetcode.com/problems/remove-k-digits/
+ * <p>
+ * Given a non-negative integer num represented as a string,
+ * remove k digits from the number so that the new number is the smallest possible.
+ * <p>
+ * Note:
+ * The length of num is less than 10002 and will be ≥ k.
+ * The given num does not contain any leading zero.
+ */
+public class RemoveKDigits {
+
+	class Solution {
+
+		public String removeKdigits(String num, int k) {
+			if (num.length() == k) {
+				return "0";
+			}
+			Deque<Character> deque = new ArrayDeque<>();
+			for (int i = 0; i < num.length(); i++) {
+				char ch = num.charAt(i);
+				while (!deque.isEmpty() && deque.peek() - ch > 0 && k > 0) {
+					deque.poll();
+					k--;
+				}
+				deque.push(ch);
+			}
+			for (; k > 0; k--) {
+				deque.pop();
+			}
+			while (!deque.isEmpty() && deque.peekLast() == '0') {
+				deque.removeLast();
+			}
+			if (deque.isEmpty()) {
+				return "0";
+			}
+			StringBuilder builder = new StringBuilder();
+			while (!deque.isEmpty()) {
+				builder.append(deque.poll());
+			}
+			return builder.reverse().toString();
+		}
+	}
+
+}
---
+++ src/test/groovy/com/algorithm/playground/leetcode/problems/lc400/lc402/RemoveKDigitsSpec.groovy
@@ -0,0 +1,51 @@
+package com.algorithm.playground.leetcode.problems.lc400.lc402
+
+import com.algorithm.playground.leetcode.problems.lc400.lc402.RemoveKDigits.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Example 1:
+ *
+ * Input: num = "1432219", k = 3
+ * Output: "1219"
+ * Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+ *
+ *
+ * Example 2:
+ *
+ * Input: num = "10200", k = 1
+ * Output: "200"
+ * Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+ *
+ *
+ * Example 3:
+ *
+ * Input: num = "10", k = 2
+ * Output: "0"
+ * Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+ */
+class RemoveKDigitsSpec extends Specification {
+
+	@Unroll
+	def "removeKdigits"() {
+
+		expect:
+
+			new S().removeKdigits(nums, k) == out
+
+		where:
+
+			nums      | k || out
+			"1432219" | 3 || "1219"
+			"10200"   | 1 || "200"
+			"10"      | 2 || "0"
+			"9"       | 1 || "0"
+			"112"     | 1 || "11"
+			"1111111" | 3 || "1111"
+			"4321"    | 2 || "21"
+			"1107"    | 1 || "107"
+
+	}
+
+}
