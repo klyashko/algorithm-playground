8e3112b78141d337a3c500687630cf190d55bbb6
kipill <kipill_90@mail.ru>
2019-08-05 21:47:49 +0200

    438. Find All Anagrams in a String

---
+++ src/main/java/com/algorithm/playground/leetcode/problems/lc400/lc438/FindAllAnagramsInAString.java
@@ -0,0 +1,58 @@
+package com.algorithm.playground.leetcode.problems.lc400.lc438;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/find-all-anagrams-in-a-string/
+ * <p>
+ * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+ * <p>
+ * Strings consists of lowercase English letters only and the
+ * length of both strings s and p will not be larger than 20,100.
+ * <p>
+ * The order of output does not matter.
+ */
+public class FindAllAnagramsInAString {
+
+	class Solution {
+
+		public List<Integer> findAnagrams(String s, String p) {
+			if (p.length() > s.length()) {
+				return Collections.emptyList();
+			}
+			int[] pattern = new int[26];
+			for (int i = 0; i < p.length(); i++) {
+				pattern[p.charAt(i) - 'a']++;
+			}
+			List<Integer> result = new ArrayList<>();
+			for (int i = 0; i < p.length() - 1; i++) {
+				pattern[s.charAt(i) - 'a']--;
+			}
+
+			for (int i = p.length() - 1; i < s.length(); i++) {
+				int le = i - p.length();
+				if (le >= 0) {
+					pattern[s.charAt(le) - 'a']++;
+				}
+				pattern[s.charAt(i) - 'a']--;
+				if (allZeros(pattern)) {
+					result.add(le + 1);
+				}
+			}
+			return result;
+		}
+
+		private boolean allZeros(int[] nums) {
+			for (int i : nums) {
+				if (i != 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+	}
+
+}
---
+++ src/test/groovy/com/algorithm/playground/leetcode/problems/lc400/lc438/FindAllAnagramsInAStringSpec.groovy
@@ -0,0 +1,52 @@
+package com.algorithm.playground.leetcode.problems.lc400.lc438
+
+import com.algorithm.playground.leetcode.problems.lc400.lc438.FindAllAnagramsInAString.Solution as S
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Example 1:
+ *
+ * Input:
+ * s: "cbaebabacd" p: "abc"
+ *
+ * Output:
+ * [0, 6]
+ *
+ * Explanation:
+ * The substring with start index = 0 is "cba", which is an anagram of "abc".
+ * The substring with start index = 6 is "bac", which is an anagram of "abc".
+ *
+ *
+ * Example 2:
+ *
+ * Input:
+ * s: "abab" p: "ab"
+ *
+ * Output:
+ * [0, 1, 2]
+ *
+ * Explanation:
+ * The substring with start index = 0 is "ab", which is an anagram of "ab".
+ * The substring with start index = 1 is "ba", which is an anagram of "ab".
+ * The substring with start index = 2 is "ab", which is an anagram of "ab".
+ */
+class FindAllAnagramsInAStringSpec extends Specification {
+
+	@Unroll
+	def "findAnagrams"() {
+
+		expect:
+
+			new S().findAnagrams(s, p) == out
+
+		where:
+
+			s            | p               || out
+			"cbaebabacd" | "abc"           || [0, 6]
+			"abab"       | "ab"            || [0, 1, 2]
+			"aaaaaaaaaa" | "aaaaaaaaaaaaa" || []
+
+	}
+
+}
