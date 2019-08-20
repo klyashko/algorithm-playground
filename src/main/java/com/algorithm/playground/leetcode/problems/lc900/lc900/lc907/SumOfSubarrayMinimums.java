181f9ee4d4190a4f5a08d1e998862160c631d281
kipill <kipill_90@mail.ru>
2019-08-20 23:46:07 +0200

    907. Sum of Subarray Minimums

--- src/main/java/com/algorithm/playground/leetcode/problems/lc900/lc900/lc907/SumOfSubarrayMinimums.java
+++ src/main/java/com/algorithm/playground/leetcode/problems/lc900/lc900/lc907/SumOfSubarrayMinimums.java
@@ -1,11 +1,36 @@
 package com.algorithm.playground.leetcode.problems.lc900.lc900.lc907;
 
+import java.util.LinkedList;
+
 /**
  * https://leetcode.com/problems/sum-of-subarray-minimums/
  */
 public class SumOfSubarrayMinimums {
 
 	class Solution {
+
+		public int sumSubarrayMins(int[] A) {
+			int MOD = 1000000007;
+			LinkedList<Integer> stack = new LinkedList<>();
+			long ans = 0;
+			for (int r = 0; r <= A.length; r++) {
+
+				int val = r == A.length ? 0 : A[r];
+
+				while (!stack.isEmpty() && A[stack.peek()] > val) {
+					int m = stack.pop();
+					int l = stack.isEmpty() ? -1 : stack.peek();
+					ans += A[m] * (m - l) * (r - m);
+					ans %= MOD;
+				}
+				stack.push(r);
+			}
+			return (int) ans;
+		}
+
+	}
+
+	class Solution_ {
 
 		public int sumSubarrayMins(int[] A) {
 			return (int) sumSubarrayMins(A, 0, A.length);
